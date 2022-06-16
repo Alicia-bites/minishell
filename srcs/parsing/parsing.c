ebb06e933dc6fec89e50b594dc124dad3aaa5d0f
alicia <alicia.mrchd@gmail.com>
2022-06-16 19:04:28 +0200

    reduce file to one function

---
+++ srcs/parsing/parsing.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_1.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 10:49:49 by amarchan          #+#    #+#             */
+/*   Updated: 2022/06/16 18:15:33 by amarchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+// Parse arguments and return inputs_list(s)
+char	*ft_parse(char *str)
+{	
+	char	**inputs_list;
+	// int		i;
+	
+	// i = 0;
+	inputs_list = ft_split(str, 32);
+	//FOR DEBUG
+	// while (inputs_list[i])
+	// 	printf("%s\n", inputs_list[i++]);
+	// puts("-------------------------------------------------------------");
+	//END
+	sort_inputs(inputs_list);
+	return (0);
+}
