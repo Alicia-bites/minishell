1d198a6a044bbd956e723c54bb9abe78ae7dd3d8
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-06-17 16:38:09 +0200

    UPDT - pwd function
    BODY:
    N/A
    
    TODO:
    Handle error
    
    modified:   pwd.c

--- srcs/built-in/pwd.c
+++ srcs/built-in/pwd.c
@@ -6,13 +6,27 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:19:28 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/16 18:23:37 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/17 16:29:50 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include <limits.h>
 
 int	do_pwd(void)
 {
+	char	*path;
+
+	path = getcwd(NULL, 0);
+	if (!path)
+		return (1);
+	printf("%s\n", path);
+	free(path);
 	return (0);
 }
+/*
+int main(void)
+{
+	do_pwd();
+	return (0);
+}*/
