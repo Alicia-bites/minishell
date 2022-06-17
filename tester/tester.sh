84645ae1e952db82478ce61589e7b49879069d8f
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-06-17 14:25:55 +0200

    UPDT - tester script and use cases files
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   tester/tester.sh
    	modified:   tester/use_case/echo.txt
    	new file:   tester/use_case/ls.txt

--- tester/tester.sh
+++ tester/tester.sh
@@ -2,6 +2,7 @@
 
 SEP_P="####################"
 SEP_SP="--------------------"
+RES_PRE="-->"
 
 BK_RD='\033[31;7m'
 BK_GN='\033[32;7m'
@@ -18,24 +19,53 @@ UC_P=${ORI_P}/use_case
 RES_P=${ORI_P}/result
 RES_ORI=${RES_P}/result_ori
 
-#while read -r line
-#do
-#  echo "$line"
-#done < "$input"
 
-function	init_all_files
+
+function	exec_test
 {
+	echo "exec test"
+	for i in ${1}/*.txt
+	do
+		echo "file name: ${i}"
+		while read -r line
+		do
+			if [ "${line}" != "#" ]
+			then
+				echo "command: ${line}"
+				eval ${line}
+				echo "${SEP_SP}"
+			fi
+		done < ${i}
+		echo ""
+		echo "${SEP_P}"
+	done
+}
+
+function	init_dir
+{
+	echo "initialisation of directory"
 	for i in $@
 	do
-		echo ${i}
-#		if [ ]
-#		then
-#		fi
+		echo "initialisation of the directory: ${i}"
+		if [ ! -e ${i} ]
+		then
+			mkdir ${i}
+			if [ ! -e ${i} ]
+			then
+				echo "${RES_PRE} error on directory creation"
+			else
+				echo "${RES_PRE} directory created"
+			fi
+		else
+			echo "directory already exists"
+		fi
 	done
+	echo "${SEP_P}"
 }
 
 function	check_dir
 {
+	echo "checking directory"
 	for i in $@
 	do
 		if [ ! -e ${i} ]
@@ -46,16 +76,19 @@ function	check_dir
 			echo "directory ${i} existing"
 		fi
 	done
+	echo "${SEP_P}"
 }
 
 function	main
 {
 	clear && echo "Terminal cleared"
+	echo "${SEP_P}"
 	if check_dir ${UC_P}; [ $? != 0 ]
 	then
 		exit 1
 	fi
-	#init_all_files ${RES_P} ${RES_ORI}
+	init_dir ${RES_P}
+	exec_test ${UC_P} ${RES_ORI}
 	exit 0
 }
 
