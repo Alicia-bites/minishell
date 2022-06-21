b2834a6e588ec7035c5e8dc0f5f2d4039e7b02f8
Alicia MARCHAND <amarchan@e3r13p12.clusters.42paris.fr>
2022-06-21 11:09:07 +0200

    ADD - added 2 functions to prevent spaces and empty str
    BODY:
    added 2 functions to prevent spaces, empty strings and clone strings to be
    added to history.
    
    TODO:
    N/A
    
    	modified:   srcs/main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,37 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/17 14:42:33 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/21 11:08:37 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	is_not_clone(char *str)
+{
+	static char	*prev_str = NULL;
+	
+	if (ft_strcmp(prev_str, str) == 0)
+	{
+		prev_str = str;
+		return (0);
+	}
+	prev_str = str;
+	return (1);
+}
+
+int	is_not_empty(char *str)
+{
+	// printf("str_2 = %s\n", str);
+	if (str == 0 || !ft_strcmp(" ", str) || !ft_strcmp("\f", str)
+		|| !ft_strcmp("\n", str) || !ft_strcmp("\r", str)
+		|| !ft_strcmp("\t", str) || !ft_strcmp("\v", str))	
+		{
+			puts("hey you");
+			return (0);			
+		}
+	return (1);
+}
 
 int	get_input(void)
 {
@@ -25,7 +51,8 @@ int	get_input(void)
 		{
 			// printf("%s\n", str);
 			ft_parse(str);
-			add_history(str);
+			if (is_not_clone(str) && is_not_empty(str))
+				add_history(str);
 		}
 	}
 	return (0);
