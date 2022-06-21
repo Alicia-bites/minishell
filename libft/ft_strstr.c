f8478217af8e2423ef16e8ba19e86e8fdb5852fb
Alicia MARCHAND <amarchan@e3r13p12.clusters.42paris.fr>
2022-06-21 11:37:48 +0200

    ADD - ft_strstr to libft
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   libft/Makefile
    	new file:   libft/ft_strstr.c
    	modified:   libft/libft.h

---
+++ libft/ft_strstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 11:29:09 by amarchan          #+#    #+#             */
+/*   Updated: 2022/06/21 11:31:10 by amarchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *big, const char *little)
+{
+	size_t	i;
+	size_t	j;
+	size_t	n;
+	char	*a;
+
+	i = 0;
+	a = (char *)big;
+	n = ft_strlen(little);
+	if (n == 0 || little == big)
+		return (a);
+	while (a[i])
+	{
+		j = 0;
+		while (a[i + j] && little[j] && a[i + j] == little[j])
+			j++;
+		if (j == n)
+			return ((char *)&a[i]);
+		i++;
+	}
+	return (0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 08:04:38 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/20 17:15:46 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/21 11:31:21 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *big, const char *little);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
