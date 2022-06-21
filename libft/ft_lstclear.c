04dd4a0420ef220a533752a225c0a2ec48986ec2
Alicia MARCHAND <amarchan@e3r13p12.clusters.42paris.fr>
2022-06-21 18:40:43 +0200

    UPDT - delete second arg
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   libft/ft_lstclear.c

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,24 +6,26 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 19:19:20 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/20 12:52:44 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/21 18:31:23 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+// #include "../includes/minishell.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst)
 {
+	// extern	g_global;
 	t_list	*iterator;
 	t_list	*prev;
-
+	
+	// g_global->lst_cleared = 1;
 	iterator = *lst;
 	prev = NULL;
 	while (iterator)
 	{
 		prev = iterator;
 		iterator = iterator->next;
-		del(prev->token);
 		free(prev);
 	}
 	*lst = NULL;
