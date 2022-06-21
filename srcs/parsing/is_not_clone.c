926105ca1547ed9f7504808184a20480cd7edd57
Alicia MARCHAND <amarchan@e3r13p12.clusters.42paris.fr>
2022-06-21 11:14:33 +0200

    ATOM - tokenizer, is_not_clone, is_not_empty
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   ../main.c
    	new file:   is_not_clone.c
    	new file:   is_not_empty.c

--- srcs/parsing/is_not_clone.c
+++ srcs/parsing/is_not_clone.c
@@ -1,39 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   is_not_clone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/21 11:14:17 by amarchan         ###   ########.fr       */
+/*   Created: 2022/06/21 11:12:56 by amarchan          #+#    #+#             */
+/*   Updated: 2022/06/21 11:13:37 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-int	get_input(void)
+int	is_not_clone(char *str)
 {
-	char	*str;
-
-	while (42)
+	static char	*prev_str = NULL;
+	
+	if (ft_strcmp(prev_str, str) == 0)
 	{
-		str = readline("prompt: ");
-		if (!str)
-			return (0);
-		if (str[0] != 0)
-		{
-			// printf("%s\n", str);
-			ft_parse(str);
-			if (is_not_clone(str) && is_not_empty(str))
-				add_history(str);
-		}
+		prev_str = str;
+		return (0);
 	}
-	return (0);
-}
-
-int	main(void)
-{
-	get_input();
-	return (0);
+	prev_str = str;
+	return (1);
 }
--- srcs/parsing/is_not_empty.c
+++ srcs/parsing/is_not_empty.c
@@ -1,39 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   is_not_empty.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/21 11:14:17 by amarchan         ###   ########.fr       */
+/*   Created: 2022/06/21 11:13:11 by amarchan          #+#    #+#             */
+/*   Updated: 2022/06/21 11:13:40 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-int	get_input(void)
+int	is_not_empty(char *str)
 {
-	char	*str;
-
-	while (42)
-	{
-		str = readline("prompt: ");
-		if (!str)
-			return (0);
-		if (str[0] != 0)
+	// printf("str_2 = %s\n", str);
+	if (str == 0 || !ft_strcmp(" ", str) || !ft_strcmp("\f", str)
+		|| !ft_strcmp("\n", str) || !ft_strcmp("\r", str)
+		|| !ft_strcmp("\t", str) || !ft_strcmp("\v", str))	
 		{
-			// printf("%s\n", str);
-			ft_parse(str);
-			if (is_not_clone(str) && is_not_empty(str))
-				add_history(str);
+			puts("hey you");
+			return (0);			
 		}
-	}
-	return (0);
-}
-
-int	main(void)
-{
-	get_input();
-	return (0);
+	return (1);
 }
