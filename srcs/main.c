8b2c663119cd45678011b77e50589ce639d25e44
Alicia MARCHAND <amarchan@e3r13p12.clusters.42paris.fr>
2022-06-21 18:41:10 +0200

    ADD - global variable + function that clears list after exec cmd
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,25 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/21 16:58:41 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/21 18:34:31 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+t_global	*g_global;
+
 int	get_input(void)
 {
 	char	*str;
-
+	t_list	*token_list;
+	
+	g_global = malloc(sizeof(t_global));
+	if (!g_global)
+	{
+		ft_panic(MALLOC_FAILURE, 0);
+		return (0);
+	}
 	while (42)
 	{
 		str = readline("$: ");
@@ -24,7 +33,11 @@ int	get_input(void)
 		if (str[0] != 0)
 		{
 			// printf("%s\n", str);
-			ft_parse(str);
+			ft_parse(str, &token_list);
+			print_lst(token_list);
+			// execute_command(token_list);
+			ft_lstclear(&token_list);
+			g_global->list_cleared = 1;
 			if (is_not_clone(str) && is_not_empty(str))
 				add_history(str);
 		}
