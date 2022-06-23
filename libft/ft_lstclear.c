8b547cc0880da1145f0a0115e7baa4c8ae9acf4b
Alicia MARCHAND <amarchan@e3r13p12.42.fr>
2022-06-23 10:28:02 +0200

    NOTHING TO DECLARE
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   libft/ft_lstclear.c

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,20 +6,17 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 19:19:20 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/21 18:31:23 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/23 09:52:58 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include "../includes/minishell.h"
 
 void	ft_lstclear(t_list **lst)
 {
-	// extern	g_global;
 	t_list	*iterator;
 	t_list	*prev;
 	
-	// g_global->lst_cleared = 1;
 	iterator = *lst;
 	prev = NULL;
 	while (iterator)
