be83d7c4851759702cad3b3cf4e1460d75ebac30
alicia <alicia.mrchd@gmail.com>
2022-06-24 15:54:44 +0200

    ADD - handle case: ${LANG}monkey
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/insert_expansions.c

--- srcs/parsing/expansions/insert_expansions.c
+++ srcs/parsing/expansions/insert_expansions.c
@@ -6,11 +6,26 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 19:49:04 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/24 14:33:56 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/24 15:53:54 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static char	*malloc_newstr(int full_size,  t_expanded **expanded_list)
+{
+	char	*new_str;
+		
+	new_str = malloc(sizeof(char) * full_size + 1);
+	if (!new_str)
+	{
+		ft_lstclear(expanded_list);
+		ft_panic(MALLOC_FAILURE, 0);
+		return (NULL);
+	}
+	ft_bzero(new_str, sizeof(char) * (full_size + 1));
+	return (new_str);
+}
 
 char	*insert_expansions(int full_size, t_expanded *expanded_list, char *str)
 {
@@ -19,16 +34,11 @@ char	*insert_expansions(int full_size, t_expanded *expanded_list, char *str)
 	int		k;
 	char	*new_str;
 
-	new_str = malloc(sizeof(char) * full_size + 1);
-	if (!new_str)
-	{
-		ft_panic(MALLOC_FAILURE, 0);
-		return (NULL);
-	}
-	ft_bzero(new_str, sizeof(char) * (full_size + 1));
+	new_str = malloc_newstr(full_size, &expanded_list);
 	i = 0;
 	j = 0;
 	k = 0;
+	printf("full size = %d\n", full_size);
 	while (i <= full_size)
 	{
 		if (str[k] == '$')
@@ -39,7 +49,11 @@ char	*insert_expansions(int full_size, t_expanded *expanded_list, char *str)
 			expanded_list = expanded_list->next;
 			k++;
 			while (ft_isalnum(str[k]) || str[k] == '{' || str[k] == '}')
-				k++;				
+			{
+				if (str[k - 1] == '}' && ft_isalnum(str[k]))
+					break;
+				k++;
+			}
 		}
 		new_str[i++] = str[k++];
 	}
