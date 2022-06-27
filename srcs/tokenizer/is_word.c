22c2415488c1dfbdeda8464f57d459aa1326e840
alicia <alicia.mrchd@gmail.com>
2022-06-27 09:47:21 +0200

    UPDT - added condition for echo -n
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/is_word.c

--- srcs/tokenizer/is_word.c
+++ srcs/tokenizer/is_word.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:10:26 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/21 17:41:39 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/25 19:04:22 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@ t_list	*is_word(t_chartype *input_list, int *start, int *end)
 	if (input_list[*end].type == CH_WORD)
 	{
 		while (input_list[*end].type == CH_WORD)
-			(*end)++;
+		{
+			if (input_list[*end + 1].type == CH_SPACE
+				&& input_list[*end + 2].character == '-'
+				&& input_list[*end + 3].character == 'n')
+					(*end) += 3;
+			(*end)++;			
+		}
 		token_list = built_token(input_list, *start, *end);
 		*start = *end;
 	}
