6c9a978c19cc4a48f3c90a80e19dddbc6846b8ec
Anthony BARRIER <abarrier@e1r7p8.42.fr>
2022-06-27 12:41:01 +0200

    UPDT - complete function
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   env.c

--- srcs/built-in/env.c
+++ srcs/built-in/env.c
@@ -6,13 +6,31 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:22:05 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/16 18:23:18 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/27 08:53:24 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	do_env(char *str)
+int	do_env(char **envp)
 {
+	int	i;
+
+	i = 0;
+	if (!envp)
+		return (1);
+	while (envp[i])
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
 	return (0);
 }
+/*
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+
+	return (do_env(envp));
+}*/
