23480c29937379334950486b35978341459fd9b1
Alicia MARCHAND <amarchan@e1r8p8.42.fr>
2022-06-27 15:04:00 +0200

    NOTHING TO DECLARE
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/execute_commands/execute_command.c

--- srcs/execute_commands/execute_command.c
+++ srcs/execute_commands/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:15:18 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/17 12:10:12 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/27 12:44:57 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 // Indicate which action should be taken
 int	execute_command(char *str, int i)
 {
-	if (i == 0)
-		do_echo(str);
-	if (i == 1)
-		do_echo_n(str);
-	if (i == 2)
-		do_cd(str);
-	if (i == 3)
-		do_pwd();
-	if (i == 4)
-		do_export(str);
-	if (i == 5)
-		do_unset(str);
-	if (i == 6)
-		do_env(str);
-	if (i == 7)
-		do_exit();
+	// if (i == 0)
+	// 	do_echo(str);
+	// if (i == 1)
+	// 	do_echo_n(str);
+	// if (i == 2)
+	// 	do_cd(str);
+	// if (i == 3)
+	// 	do_pwd();
+	// if (i == 4)
+	// 	do_export(str);
+	// if (i == 5)
+	// 	do_unset(str);
+	// if (i == 6)
+	// 	do_env(str);
+	// if (i == 7)
+	// 	do_exit();
 	return (0);
 }
