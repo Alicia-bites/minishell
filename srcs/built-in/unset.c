34406cfaf6c028cfff63c917621b764c443c448e
Anthony BARRIER <abarrier@e1r7p8.42.fr>
2022-06-27 12:42:25 +0200

    UPDT - starting to work on it and check bash behaviour
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   unset.c

--- srcs/built-in/unset.c
+++ srcs/built-in/unset.c
@@ -6,12 +6,20 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:21:21 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/16 18:21:49 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/27 09:20:40 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/*
+ * Test
+ * export ANTHO=test
+ * unset ANTHO ==> Ok, always return(0)
+ * unset "ANTHO" ==> Ok, always return(0)
+ * unset 'ANTHO' ==> Ok, always return(0)
+ * unset $ANTHO
+ */
 int	do_unset(char *str)
 {
 	return (0);
