11dafa070e84b74bc04a13e2a345c56c2d628bb1
BarrierAntho <barrier.antho@gmail.com>
2022-06-28 21:55:54 +0200

    TEST - env list and functions
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         srcs/main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/27 17:49:53 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/28 19:26:47 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,37 @@ int	get_input(void)
 	str = NULL;
 	while (42)
 	{
-		str = readline("$: ");
-		if (!str)
-			return (0);
-		if (str[0] != 0)
-		{
-			ft_parse(str, &token_list);
-			// print_lst(token_list);
-			// execute_command(token_list);
-			ft_lstclear(&token_list);
-			// printf("token_list (main) = %p\n", token_list);
-			if (is_not_clone(str) && is_not_empty(str))
-				add_history(str);
-		}
+//		str = readline("$: ");
+//		if (!str)
+//			return (0);
+//		if (str[0] != 0)
+//		{
+//			ft_parse(str, &token_list);
+//			// print_lst(token_list);
+//			// execute_command(token_list);
+//			ft_lstclear(&token_list);
+//			// printf("token_list (main) = %p\n", token_list);
+//			if (is_not_clone(str) && is_not_empty(str))
+//				add_history(str);
+//		}
 	}
 	return (0);
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
-	get_input();
+	t_ulist	**env_lst;
+
+	env_lst = ft_lst_init();
+	if (!env_lst)
+		return (1);
+	if (env_lst_set(envp, env_lst))
+	{
+		ft_lst_free(env_lst, &env_free);
+		return (2);
+	}
+	env_lst_show(env_lst);
+	ft_lst_free(env_lst, &env_free);
+	//get_input();
 	return (0);
 }
