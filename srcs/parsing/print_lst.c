1416198802db9348dc85c4ada465369b3c769bde
alicia <alicia.mrchd@gmail.com>
2022-06-28 15:04:37 +0200

    ADD - prev and next to print
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/print_lst.c

--- srcs/parsing/print_lst.c
+++ srcs/parsing/print_lst.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:11:16 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/21 16:36:01 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/28 14:59:47 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	print_lst(t_list *lst)
 		printf("index = %d\n", iterator->index);
 		printf("token = %s\n", iterator->token);
 		printf("toktype = %u\n", iterator->toktype);
+		printf("prev = %p\n", iterator->prev);
+		printf("next = %p\n", iterator->next);
 		puts("-----------------------------------------");
 		iterator = iterator->next;		
 	}
