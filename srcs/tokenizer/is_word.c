3208c60d564a9699454bd6f056976df977be3edf
alicia <alicia.mrchd@gmail.com>
2022-06-29 17:46:05 +0200

    FIX - can handle spaces directly after echo like so : echo     -n
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/is_word.c

--- srcs/tokenizer/is_word.c
+++ srcs/tokenizer/is_word.c
@@ -6,28 +6,37 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:10:26 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/29 17:31:19 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/29 17:45:27 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	echo_n(t_chartype *input_list, int end)
+int	echo_n(t_chartype *input_list, int end, int *spaces)
 {
 	if (input_list[end].character == 'e'
 		&& input_list[end + 1].character == 'c'
 		&& input_list[end + 2].character == 'h'
-		&& input_list[end + 3].character == 'o'
-		&& input_list[end + 4].type == CH_SPACE
-		&& input_list[end + 5].character == '-'
-		&& input_list[end + 6].character == 'n')
-			return (1);
+		&& input_list[end + 3].character == 'o')
+		{
+			end += 4;
+			while (input_list[end].type == CH_SPACE)
+			{
+				end++;
+				(*spaces)++;				
+			}
+			if (input_list[end].character == '-'
+				&& input_list[end + 1].character == 'n')
+				return (1);
+		}
 	return (0);
 }
 
-void	echo_special_treatment(t_chartype *input_list, int *start, int *end)
+void	echo_special_treatment(t_chartype *input_list, int *start, int *end,
+	int spaces)
 {
 	(*end) += 5;
+	(*end) += (spaces - 1);
 	while (input_list[*end].character == '-'
 		&& input_list[*end + 1].character == 'n')
 		{
@@ -42,19 +51,19 @@ void	echo_special_treatment(t_chartype *input_list, int *start, int *end)
 void	is_word(t_chartype *input_list, int *start, int *end,
 	t_list **token_list)
 {	
-	int	echo;
+	int	spaces;
 	
-	echo = 0;
+	spaces = 0;
 	if (input_list[*end].type == CH_WORD)
 	{
 		while (input_list[*end].type == CH_WORD)
 		{
-			if (echo_n(input_list, *end))
+			if (echo_n(input_list, *end, &spaces))
 			{
-				echo_special_treatment(input_list, start, end);
-				echo = 1;				
+				echo_special_treatment(input_list, start, end, spaces);
+				spaces = 1;				
 			}
-			if (echo == 1)
+			if (spaces)
 				break ;
 			(*end)++;
 		}
