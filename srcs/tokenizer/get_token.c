1f01d26b1c1ce119d0acc7b33ce96799c4466ae6
alicia <alicia.mrchd@gmail.com>
2022-06-30 11:17:35 +0200

    FIX - now don't ommit the space betweeen commands
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/get_token.c
    	modified:   srcs/tokenizer/is_word.c
    	modified:   tester/use_case/echo.txt

--- srcs/tokenizer/get_token.c
+++ srcs/tokenizer/get_token.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:58:04 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/29 15:00:28 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/30 11:11:21 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	get_token(t_chartype *input_list, t_list **token_list)
 
 	start = 0;
 	end = 0;
-	while (start < (input_list[start].length - 1))
+	while (start < input_list[start].length)
 	{
 		is_word(input_list, &start, &end, token_list);
 		is_space(input_list, &start, &end, token_list);
--- srcs/tokenizer/is_word.c
+++ srcs/tokenizer/is_word.c
@@ -6,13 +6,13 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:10:26 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/29 17:45:27 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/30 11:14:13 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	echo_n(t_chartype *input_list, int end, int *spaces)
+static int	echo_n(t_chartype *input_list, int end, int *spaces)
 {
 	if (input_list[end].character == 'e'
 		&& input_list[end + 1].character == 'c'
@@ -25,15 +25,15 @@ int	echo_n(t_chartype *input_list, int end, int *spaces)
 				end++;
 				(*spaces)++;				
 			}
-			if (input_list[end].character == '-'
+			if (input_list[end].character == '-' 
 				&& input_list[end + 1].character == 'n')
 				return (1);
 		}
 	return (0);
 }
 
-void	echo_special_treatment(t_chartype *input_list, int *start, int *end,
-	int spaces)
+static void	echo_special_treatment(t_chartype *input_list, int *start,
+	int *end, int spaces)
 {
 	(*end) += 5;
 	(*end) += (spaces - 1);
@@ -46,6 +46,14 @@ void	echo_special_treatment(t_chartype *input_list, int *start, int *end,
 			while (input_list[*end].type == CH_SPACE)
 				(*end)++;
 		}
+}
+
+void	handle_echo(t_list **token_list)
+{
+	char *token;
+	
+	token = "echo -n";
+	add_token_to_list(token, token_list);
 }
 
 void	is_word(t_chartype *input_list, int *start, int *end,
@@ -61,13 +69,17 @@ void	is_word(t_chartype *input_list, int *start, int *end,
 			if (echo_n(input_list, *end, &spaces))
 			{
 				echo_special_treatment(input_list, start, end, spaces);
-				spaces = 1;				
+				(*end)--;
 			}
 			if (spaces)
-				break ;
+			{
+				// printf(" input_list[*end].character = %c\n", input_list[*end].character);
+				handle_echo(token_list);
+				*start = (*end);
+				return ;
+			}
 			(*end)++;
 		}
-		// printf("end = %d\n", *end);
 		built_token(input_list, *start, *end, token_list);
 		*start = *end;
 	}
