9311977cd580a93e7af48e3ec685fce143e96be0
alicia <alicia.mrchd@gmail.com>
2022-06-30 15:50:08 +0200

    ADD - handling properly echo -nnnnnn -nnnn -nnnalicia
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/is_word.c

--- srcs/tokenizer/is_word.c
+++ srcs/tokenizer/is_word.c
@@ -6,74 +6,119 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:10:26 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/30 11:20:15 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/06/30 15:48:10 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	echo_n(t_chartype *input_list, int end, int *spaces)
+static int	echo_n(t_chartype *input_list, int *end)
 {
-	if (input_list[end].character == 'e'
-		&& input_list[end + 1].character == 'c'
-		&& input_list[end + 2].character == 'h'
-		&& input_list[end + 3].character == 'o')
+	int	tmp;
+
+	tmp = *end;
+	if (input_list[*end].character == 'e'
+		&& input_list[*end + 1].character == 'c'
+		&& input_list[*end + 2].character == 'h'
+		&& input_list[*end + 3].character == 'o')
 		{
-			end += 4;
-			while (input_list[end].type == CH_SPACE)
+			(*end) += 4;
+			while (input_list[*end].type == CH_SPACE)
 			{
-				end++;
-				(*spaces)++;				
+				(*end)++;
 			}
-			if (input_list[end].character == '-' 
-				&& input_list[end + 1].character == 'n')
+			while (input_list[*end].type == CH_D_QUOTE
+				|| input_list[*end].type == CH_S_QUOTE)
+				{
+					(*end)++;
+				}
+			if (input_list[*end].character == '-')
+				(*end)++;
+			while (input_list[*end].type == CH_D_QUOTE
+				|| input_list[*end].type == CH_S_QUOTE)
+				{
+					(*end)++;
+				}
+			if (input_list[*end].character == 'n')
 				return (1);
 		}
+	*end = tmp;
 	return (0);
 }
 
 static void	echo_special_treatment(t_chartype *input_list, int *start,
-	int *end, int spaces)
+	int *end, int *space)
 {
-	(*end) += 5;
-	(*end) += (spaces - 1);
+	(*end) -= 1;
 	while (input_list[*end].character == '-'
 		&& input_list[*end + 1].character == 'n')
 		{
 			(*end)++;
 			while (input_list[*end].character == 'n')
 				(*end)++;
-			while (input_list[*end].type == CH_SPACE)
+			while (input_list[*end].type == CH_D_QUOTE
+				|| input_list[*end].type == CH_S_QUOTE)
+			{
 				(*end)++;
+			}
+			if (input_list[*end].character == '-')
+			{
+				*space = 0;
+			}
+			while (input_list[*end].type == CH_SPACE)
+			{
+				if (!(*space))
+					break;
+				(*end)++;
+			}
+			while (input_list[*end].type == CH_D_QUOTE
+				|| input_list[*end].type == CH_S_QUOTE)
+			{
+				(*end)++;
+			}
+			if (input_list[*end].character != '-'
+				&& input_list[*end + 1].character != 'n'
+				&& (input_list[*end].character != '"'
+				|| input_list[*end].type != CH_SPACE))
+				{
+					while (input_list[*end].type != CH_SPACE)
+						(*end)--;
+					return ;					
+				}
 		}
 }
 
-static void	built_echo(t_list **token_list)
+static void	built_echo(t_list **token_list, int space)
 {
 	char *token;
 	
-	token = "echo -n";
+	if (!space)
+		token = "echo";
+	else
+		token = "echo -n";
 	add_token_to_list(token, token_list);
 }
 
 void	is_word(t_chartype *input_list, int *start, int *end,
 	t_list **token_list)
 {	
-	int	spaces;
-	
-	spaces = 0;
+	int space;
+
+	space = 1;
 	if (input_list[*end].type == CH_WORD)
 	{
 		while (input_list[*end].type == CH_WORD)
 		{
-			if (echo_n(input_list, *end, &spaces))
+			if (echo_n(input_list, end))
 			{
-				echo_special_treatment(input_list, start, end, spaces);
-				built_echo(token_list);
-				*start = (*end)--;
+				echo_special_treatment(input_list, start, end, &space);
+				built_echo(token_list, space);
+				if (!space)
+					*start = *start + 5;
+				else
+					*start = *end;
 				return ;
 			}
-
 			(*end)++;
 		}
 		built_token(input_list, *start, *end, token_list);
