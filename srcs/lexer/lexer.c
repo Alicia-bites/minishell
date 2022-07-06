0ca854350dfea319dd5bb2470797e051714639cc
alicia <alicia.mrchd@gmail.com>
2022-07-06 10:48:47 +0200

    UPDT - cleaned printf
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/lexer/lexer.c

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 13:52:57 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/06 10:46:13 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/06 10:48:42 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,10 +98,7 @@ int	lexer(char *str)
 	err = 0;
 	if (lex_quote(str, &err) || lex_pipe(str, &err) || lex_redir(str, &err)
 		|| lex_sym(str, &err))
-		{
-			printf("err = %d\n", err);
 			return (err);
-		}
 		return (err);
 	return (0);
 }
