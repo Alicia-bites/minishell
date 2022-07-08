947fdbe074d7122b50a585ed94f261af9ae01151
alicia <alicia.mrchd@gmail.com>
2022-07-08 16:01:24 +0200

    ATOM - starting to split function
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/get_toktype.c

--- srcs/tokenizer/get_toktype.c
+++ srcs/tokenizer/get_toktype.c
@@ -6,39 +6,41 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:58:10 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/05 17:03:31 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:47:07 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static void	is_built_in(t_list **token_list)
+{
+	int	i;
+	char **built_ins;
+
+	built_ins = store_built_ins();
+	i = 0;
+	while (i < 8)
+	{
+		if (!ft_strcmp((*token_list)->token, built_ins[i++]))
+		{
+			(*token_list)->toktype = TOK_BUILTIN;
+			break ;				
+		}
+	}
+}
+
 void	get_toktype(t_chartype *input_list, t_list **token_list)
 {
 	t_list	*it;
-	int		i;
-	char 	**built_ins;
 	
 	it = *token_list;
-	built_ins = store_built_ins();
 	while (*token_list)
 	{
-		i = 0;
-		while (i < 8)
-		{
-			if (!ft_strcmp((*token_list)->token, built_ins[i++]))
-			{
-				(*token_list)->toktype = TOK_BUILTIN;
-				break ;				
-			}
-		}
+		is_built_in(token_list);
 		if (is_operator((*token_list)->token))
-		{
 			(*token_list)->toktype = is_operator((*token_list)->token);				
-		}
 		else if (!only_space_in_str((*token_list)->token))
-		{
 			(*token_list)->toktype = TOK_SPACE;
-		}
 		//aller chercher cette fonction dans pipex
 		// else if (ft_iscommand((*token_list)->token))
 		// {
@@ -59,5 +61,4 @@ void	get_toktype(t_chartype *input_list, t_list **token_list)
 		*token_list = (*token_list)->next;
 	}
 	*token_list = it;
-	// print_lst(*token_list);
 }
