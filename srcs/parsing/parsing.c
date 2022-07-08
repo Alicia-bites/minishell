c9db274c0a9a53519c5a54e6d650b931c05b614a
alicia <alicia.mrchd@gmail.com>
2022-07-08 15:33:01 +0200

    ADD - add lexer check after expansions
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/parsing.c

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:49:49 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/05 14:43:17 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:32:49 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	ft_parse(char *str, t_list **token_list)
 		return (err);
 	if (ft_strstr(str, "$") && (!count_single(str) || count_single(str) % 2))
 		str = expand_dollar(str, input_list);
+	if (lexer(str))
+		return (err);
 	create_input_list(&input_list, str);
 	tokenize(input_list, token_list);
 	print_lst(*token_list);
