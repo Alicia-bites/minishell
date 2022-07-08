063d8033c5bce838323174ea17275b87500adef1
alicia <alicia.mrchd@gmail.com>
2022-07-08 11:20:37 +0200

    ADD - redir at the end or the beginning of the command line
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/lexer/lex_redir.c
    	modified:   srcs/lexer/lexer.c

--- srcs/lexer/lex_redir.c
+++ srcs/lexer/lex_redir.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 10:51:02 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/06 10:51:33 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/08 11:19:55 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 int	lex_redir(char *str, int *err)
 {
 	int	i;
+	int	len;
 
 	i = 0;
+	len = ft_strlen(str);
 	while (str[i])
 	{
 		if ((str[i] == '>' && str[i + 1] == '<')
@@ -26,6 +28,8 @@ int	lex_redir(char *str, int *err)
 		&& (str[i + 2] == '>' || str[i + 2] == '<')
 		|| ((str[i] == '>' && str[i + 1] ==  '>' && str[i + 2] == '>')
 		|| str[i] == '<' && str[i + 1] == '<' && str[i + 2] == '<')
+		|| (str[0] == '<' || str[0] == '>'
+		|| str[len - 1] == '<' || str[len - 1] == '>')
 		)
 		{
 			*err = MISPLACED_REDIR;
