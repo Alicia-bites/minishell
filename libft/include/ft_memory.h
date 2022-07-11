825e6bdfd9e8a2b0c5d116729daff3e4865c9b2b
Anthony BARRIER <abarrier@e1r10p14.clusters.42paris.fr>
2022-07-11 18:42:23 +0200

    ADD - free_ptrptr_str
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   libft/Makefile
    	modified:   libft/include/ft_memory.h
    	new file:   libft/memory/ft_free_ptrptr_str.c

--- libft/include/ft_memory.h
+++ libft/include/ft_memory.h
@@ -6,14 +6,17 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 11:53:35 by abarrier          #+#    #+#             */
-/*   Updated: 2022/06/28 11:55:30 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/11 18:15:25 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MEMORY_H
 # define FT_MEMORY_H
 
+# include <stdlib.h>
+
 void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_free_ptrptr_str(char **s);
 void	*ft_memchr(const void *b, int c, size_t n);
 int		ft_memcmp(const void *b1, const void *b2, size_t n);
 void	*ft_memcpy(void *s1, const void *s2, size_t n);
