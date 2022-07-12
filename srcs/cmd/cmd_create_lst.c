4cd1606633517bf035ddfb246390a9c71ef754fe
Anthony BARRIER <abarrier@e1r4p3.clusters.42paris.fr>
2022-07-12 16:18:41 +0200

    UPDT - adding cmd construction sequence
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   cmd_create_lst.c

--- srcs/cmd/cmd_create_lst.c
+++ srcs/cmd/cmd_create_lst.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 17:45:45 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/11 18:40:28 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/12 14:03:27 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,29 @@
 int	cmd_create_lst(t_list *tok, t_ulist **env_lst, t_ulist **cmd_lst)
 {
 	t_list	*obj;
-	int	j;
+	t_cmd	*cmd;
+	int		j;
 
 	obj = tok;
 	while (obj)
 	{
-		printf("ANTHOOOOOOOOOOO\n");
-		printf("index = %d\n", obj->index);
-		if (ft_isspace(obj->token[0]))
+//		printf("ANTHOOOOOOOOOOO\n");
+//		print_token(obj);
+		cmd = NULL;
+		if (obj->toktype == TOK_BUILTIN || obj->toktype == TOK_CMD)
 		{
-			j = 0;
-			while (obj->token[j])
-				printf("%d ", obj->token[j++]);
-			printf("\n");
-		}
+			cmd = cmd_init(env_lst);
+			if (!cmd)
+				return (1);
+			obj = cmd_init_prop(obj, cmd);
+			if (!ft_lst_append(cmd_lst, (void *)cmd))
+			{
+				cmd_free(cmd);
+				return (2);
+			}
+		}	
 		else
-			printf("token = %s\n", obj->token);
-		printf("toktype = %u\n", obj->toktype);
-		// printf("prev = %p\n", obj->prev);
-		// printf("next = %p\n", obj->next);
-		puts("-----------------------------------------");
-		obj = obj->next;
+			obj = obj->next;
 	}
 	return (0);
 }
