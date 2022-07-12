23322c6e2632eea6964bc98f0a14b0adf2451c41
Alicia MARCHAND <amarchan@e1r4p1.42.fr>
2022-07-12 15:32:14 +0200

    ADD - TOK_CMD handled
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/get_toktype.c

--- srcs/tokenizer/get_toktype.c
+++ srcs/tokenizer/get_toktype.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:58:10 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/12 11:47:10 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/12 15:31:01 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	is_built_in(t_list **token_list)
 			return (1);
 		}
 	}
-	// free(built_ins);
+	free(built_ins);
 	return (0);
 }
 
@@ -39,27 +39,17 @@ static int	is_filename(t_list **token_list)
 		if ((*token_list)->prev->toktype == TOK_L_REDIR
 			|| (*token_list)->prev->toktype == TOK_R_REDIR
 			|| (*token_list)->prev->toktype == TOK_DR_REDIR)
-			{
-				// (*token_list)->toktype = TOK_FILE;
-				return (1);		
-			}
+			return (1);		
 	}
-	else if ((*token_list)->index >= 3)
+	if ((*token_list)->index >= 3)
 	{
-		if (
-			((*token_list)->prev->toktype == SPACE
+		if (((*token_list)->prev->toktype == TOK_SPACE
 			&& (*token_list)->prev->prev->toktype == TOK_L_REDIR)
-			
-			|| ((*token_list)->prev->toktype == SPACE
+			|| ((*token_list)->prev->toktype == TOK_SPACE
 			&& (*token_list)->prev->prev->toktype == TOK_R_REDIR)
-			
-			|| ((*token_list)->prev->toktype == SPACE
-			&& (*token_list)->prev->prev->toktype == TOK_DR_REDIR)
-			)
-			{
-				// (*token_list)->toktype = TOK_FILE;
+			|| ((*token_list)->prev->toktype == TOK_SPACE
+			&& (*token_list)->prev->prev->toktype == TOK_DR_REDIR))
 				return (1);		
-			}
 	}
 	return (0);
 }
@@ -71,7 +61,7 @@ static int	is_heredoc_sep(t_list **token_list)
 		if ((*token_list)->prev->toktype == TOK_DL_REDIR)
 			return (1);
 	}
-	else if ((*token_list)->index >= 3)
+	if ((*token_list)->index >= 3)
 	{
 		if ((*token_list)->prev->toktype == TOK_SPACE
 			&& (*token_list)->prev->prev->toktype == TOK_DL_REDIR)
@@ -86,16 +76,16 @@ static int	is_argument(t_list **token_list)
 	if ((*token_list)->index >= 2)
 	{
 		if ((*token_list)->prev->prev->toktype == TOK_BUILTIN
-			&& (ft_strcmp((*token_list)->token, "$")
-			/*|| (*token_list)->prev->toktype == TOK_CMD*/))
+			&& (ft_strcmp((*token_list)->token, "$"))
+			|| ((*token_list)->prev->prev->toktype == TOK_CMD))
 			(*token_list)->toktype = TOK_ARG;
 	}
 	else if ((*token_list)->index >= 3)
 	{
 		if (!(ft_strcmp((*token_list)->prev->token, "$")
-			&& (*token_list)->prev->prev->prev->toktype == TOK_BUILTIN)
-				/*|| (*token_list)->prev->toktype == TOK_CMD*/)
-			return (1);
+			&& (*token_list)->prev->prev->toktype == TOK_BUILTIN)
+			|| (*token_list)->prev->toktype == TOK_CMD)
+			(*token_list)->toktype = TOK_ARG;
 	}
 	if ((*token_list)->toktype == TOK_ARG)
 	{
@@ -107,11 +97,75 @@ static int	is_argument(t_list **token_list)
 				(*token_list)->toktype = TOK_ARG;
 			*token_list = (*token_list)->next;
 		}
-		if ((*token_list)->prev)
+		if ((*token_list))
 			*token_list = (*token_list)->prev;
+		return (1);
 	}
-
 	return (0);	
+}
+
+int	redir_token(t_list **token_list)
+{
+	if ((*token_list)->index >= 1)
+	{
+		if (((*token_list)->prev->toktype == TOK_L_REDIR
+			|| (*token_list)->prev->toktype == TOK_R_REDIR
+			|| (*token_list)->prev->toktype == TOK_DR_REDIR))
+			return (1);	
+	}
+	return (0);
+}
+
+int	token_redir(t_list **token_list)
+{
+	if ((*token_list)->next)
+	{
+		if (((*token_list)->next->toktype == TOK_L_REDIR
+			|| (*token_list)->next->toktype == TOK_R_REDIR
+			|| (*token_list)->next->toktype == TOK_DR_REDIR))
+			return (1);	
+	}
+	return (0);
+}
+
+int	redir_space_token(t_list **token_list)
+{
+	if ((*token_list)->index >= 2)
+	{
+		if (((*token_list)->prev->toktype == TOK_SPACE
+			&& (*token_list)->prev->prev->toktype == TOK_L_REDIR)
+			|| ((*token_list)->prev->toktype == TOK_SPACE
+			&& (*token_list)->prev->prev->toktype == TOK_R_REDIR)
+			|| ((*token_list)->prev->toktype == TOK_SPACE
+			&& (*token_list)->prev->prev->toktype == TOK_DR_REDIR))
+				return (1);		
+	}
+	return (0);
+}
+
+int	token_space_redir(t_list **token_list)
+{
+	if ((*token_list)->next && (*token_list)->next->next)
+	{
+		if (((*token_list)->next->toktype == TOK_SPACE
+			&& (*token_list)->next->next->toktype == TOK_L_REDIR)
+			|| ((*token_list)->next->toktype == TOK_SPACE
+			&& (*token_list)->next->next->toktype == TOK_R_REDIR)
+			|| ((*token_list)->next->toktype == TOK_SPACE
+			&& (*token_list)->next->next->toktype == TOK_DR_REDIR))
+				return (1);		
+	}
+	return (0);
+}
+
+int	is_cmd(t_list **token_list)
+{
+	if ((redir_token(token_list)
+		|| redir_space_token(token_list))
+		&& (token_redir(token_list))
+		|| token_space_redir(token_list))
+		return (0);
+	return (1);
 }
 
 void	get_toktype(t_list **token_list)
@@ -121,8 +175,9 @@ void	get_toktype(t_list **token_list)
 	it = *token_list;
 	while (*token_list)
 	{
-		is_built_in(token_list);
-		if (is_operator((*token_list)->token))
+		if (is_built_in(token_list))
+			NULL;
+		else if (is_operator((*token_list)->token))
 			(*token_list)->toktype = is_operator((*token_list)->token);
 		else if (!only_space_in_str((*token_list)->token))
 			(*token_list)->toktype = TOK_SPACE;
@@ -131,7 +186,9 @@ void	get_toktype(t_list **token_list)
 		else if (is_heredoc_sep(token_list))
 			(*token_list)->toktype = TOK_HERESEP;
 		else if (is_argument(token_list))
-			(*token_list)->toktype = TOK_ARG;
+			NULL;
+		else if (is_cmd(token_list))
+			(*token_list)->toktype = TOK_CMD;
 		if (*token_list)
 			*token_list = (*token_list)->next;
 	}
