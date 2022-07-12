e543a6b750890aa4b9e7204e89adad571de1a8cf
Anthony BARRIER <abarrier@e1r4p3.clusters.42paris.fr>
2022-07-12 16:14:46 +0200

    UPDT - rebuild function according to newly cmd list
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   cmd_init_prop.c

--- srcs/cmd/cmd_init_prop.c
+++ srcs/cmd/cmd_init_prop.c
@@ -6,20 +6,36 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 17:08:21 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/11 18:36:14 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/12 15:59:28 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	cmd_init_prop(t_cmd *cmd, t_list *tok, t_ulist **envp)
+t_list	*cmd_init_prop(t_list *tok, t_cmd *cmd)
 {
-	if (!cmd)
+	t_list	*obj;
+
+	if (!tok || !cmd)
 	{
 		ft_panic(-1, ERR_NOOBJ);
-		return (1);
+		return (NULL);
 	}
-	cmd->arg = NULL;
+	obj = tok;
+	cmd->toktype = obj->toktype;
+	cmd->n_arg = cmd_init_prop_n_arg(obj, cmd);
+	if (cmd->n_arg > 0)
+		obj = cmd_init_prop_fullcmd(obj, cmd);
+	else
+		obj = cmd_init_prop_fullcmd_null(obj, cmd);
+	if (!cmd->fullcmd)
+		return (NULL);
+	if (cmd->toktype == TOK_CMD && cmd->n_arg > 0)
+		cmd->fullpath = cmd_getvalidpath(cmd);
+	else
+		cmd->fullpath = cmd_getvalidpath_null(cmd);
+	return (obj);
+}
 //	if (*arg == '\0')
 //	{
 //		if (ppx_cmd_init_prop_null(cmd))
@@ -27,12 +43,12 @@ int	cmd_init_prop(t_cmd *cmd, t_list *tok, t_ulist **envp)
 //					0, ERR_MALLOC));
 //		return (0);
 //	}
-	cmd->fullcmd = (char **)malloc(sizeof(char *) * (1 + 1));
-	if (!cmd->fullcmd)
-	{
-		ft_panic(-1, ERR_MALLOC);
-		return (2);
-	}
+//	cmd->fullcmd = (char **)malloc(sizeof(char *) * (1 + 1));
+//	if (!cmd->fullcmd)
+//	{
+//		ft_panic(-1, ERR_MALLOC);
+//		return (2);
+//	}
 //	cmd->fullpath = cmd_getvalidpath(cmd, envp);
 //	if (cmd->fullcmd[0] != cmd->fullpath)
 //	{
@@ -45,5 +61,3 @@ int	cmd_init_prop(t_cmd *cmd, t_list *tok, t_ulist **envp)
 //			return (3);
 //		}
 //	}
-	return (0);
-}
