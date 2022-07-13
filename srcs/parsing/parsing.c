4f065dfe5ec5136994ab2a9c7890f5d270f82be9
Alicia MARCHAND <amarchan@e1r9p11.clusters.42paris.fr>
2022-07-13 14:35:59 +0200

    ADD - returning errcode to get_input()
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/parsing.c

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,25 +6,23 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:49:49 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/13 12:54:48 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/13 14:29:32 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_parse(char *str, t_list **token_list)
+int	ft_parse(char *str, t_list **token_list, int *err)
 {	
 	t_chartype	*input_list;
-	int			err;
 
-	err = 0;
 	input_list = NULL;
-	if (pre_lexer(str, &err))
-		return (err);
+	if (pre_lexer(str, err))
+		return (*err);
 	if (ft_strstr(str, "$"))
 		str = expand_dollar(str);
-	if (pre_lexer(str, &err))
-		return (err);
+	if (pre_lexer(str, err))
+		return (*err);
 	create_input_list(&input_list, str);
 	tokenize(input_list, token_list);
 	free(input_list);
