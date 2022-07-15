0149cc13e099ae5da57290f17ef4818638e41577
Anthony BARRIER <abarrier@e1r2p21.clusters.42paris.fr>
2022-07-15 15:21:46 +0200

    UPDT - ensure to create the cmd even if the env->value is NULL due to export VARIABLE_NAME without value
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   cmd_loop_envp.c
    	modified:   cmd_loop_envp_str.c

--- srcs/cmd/cmd_loop_envp_str.c
+++ srcs/cmd/cmd_loop_envp_str.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 08:05:01 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/12 17:46:23 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/15 15:06:09 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,11 @@ char	*cmd_loop_envp_str(char *cmd, t_ulist **envp_lst, char *s)
 		//printf("**%s** envp ptr = %p\t envp->fullname = %s\n)", __FILE__, envp, envp->fullname);
 		if (ft_strncmp(envp->fullname, s, ft_strlen(s)) == 0)
 		{
-			envline = ft_split(envp->value, ENV_FIELD_SEP);
+			if (!envp->value)
+				envline = ft_split("", ENV_FIELD_SEP);
+			else
+				envline = ft_split(envp->value, ENV_FIELD_SEP);
+
 			if (!envline)
 			{
 				ft_panic(-1, ERR_NOOBJ);
@@ -38,6 +42,7 @@ char	*cmd_loop_envp_str(char *cmd, t_ulist **envp_lst, char *s)
 			}
 			path = cmd_loop_envline(cmd, envline);
 			ft_free_ptrptr_str(envline);
+			envline = NULL;
 			if (path)
 				return (path);
 		}
