a1fe9216b4a50259bbf35cba89ecb6a3e119137a
Alicia MARCHAND <amarchan@e1r1p13.clusters.42paris.fr>
2022-07-15 11:07:05 +0200

    ADD - condition to make sure redir token is not first token of the line
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/give_type_to_token/is_combo_redir.c

--- srcs/tokenizer/give_type_to_token/is_combo_redir.c
+++ srcs/tokenizer/give_type_to_token/is_combo_redir.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 09:47:26 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/15 09:49:41 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/15 11:04:30 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@ int	is_combo_redir(t_list **token_list)
 {
 	if ((*token_list)->index >= 4)
 	{
-		if (((*token_list)->prev->prev->prev->prev->toktype == TOK_L_REDIR
-			|| (*token_list)->prev->prev->prev->prev->toktype == TOK_R_REDIR
-			|| (*token_list)->prev->prev->prev->prev->toktype == TOK_DR_REDIR)
-			&& ((*token_list)->prev->prev->prev->toktype == TOK_SPACE)
-			&& ((*token_list)->prev->prev->toktype == TOK_FILE)
-			&& ((*token_list)->prev->toktype == TOK_SPACE))
-				return (1);
+		if ((*token_list)->prev->prev->prev->prev->index != 0)
+		{
+			if (((*token_list)->prev->prev->prev->prev->toktype == TOK_L_REDIR
+				|| (*token_list)->prev->prev->prev->prev->toktype == TOK_R_REDIR
+				|| (*token_list)->prev->prev->prev->prev->toktype == TOK_DR_REDIR)
+				&& ((*token_list)->prev->prev->prev->toktype == TOK_SPACE)
+				&& ((*token_list)->prev->prev->toktype == TOK_FILE)
+				&& ((*token_list)->prev->toktype == TOK_SPACE))
+					return (1);
+		}
+
 	}
 	return (0);
 }
