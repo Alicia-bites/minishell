b4522252e3a9aaa2b51ab19f85f9d2920690bbcb
Alicia MARCHAND <amarchan@e1r1p13.clusters.42paris.fr>
2022-07-15 12:05:58 +0200

    ADD - if first token and not an operator, has to be a cmd
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/give_type_to_token/is_cmd.c

--- srcs/tokenizer/give_type_to_token/is_cmd.c
+++ srcs/tokenizer/give_type_to_token/is_cmd.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 19:29:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/14 19:29:42 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/15 12:05:34 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 // Is command a token that is not placed before or after a redirection.
 int	is_cmd(t_list **token_list)
 {
+	if ((*token_list)->index == 0 && !is_operator((*token_list)->token))
+		return (1);
 	if (is_combo_redir_when_redir_index_zero(token_list))
 		return (1);
 	if (cmd_redir(token_list))
