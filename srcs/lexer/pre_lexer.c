dcde9da1453dfb657406c5caed110a7b80bc80f4
Alicia MARCHAND <amarchan@e1r7p22.clusters.42paris.fr>
2022-07-18 14:03:15 +0200

    FIX - | + space at the end of line
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/lexer/pre_lexer.c

--- srcs/lexer/pre_lexer.c
+++ srcs/lexer/pre_lexer.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 13:52:57 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/18 13:43:39 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/18 14:02:43 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	lex_space(char *str, int *err)
 {
-	if (ft_isspace(str[0]) && (str[1] == '|'
-		|| ft_isspace(str[ft_strlen(str) - 2]) && str[ft_strlen(str - 1)]))
+	if (ft_isspace(str[0]) && str[1] == '|'
+		|| (str[ft_strlen(str) - 2]) == '|' && ft_isspace(str[ft_strlen(str) - 1]))
 		{
 			*err = MISPLACED_PIPE;
 			printf("smbash: syntax error. Please check pipes.\n");
