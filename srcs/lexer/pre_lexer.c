cc59ea444667a719500c1a7fe6d6a0c08a6ddbcd
Alicia MARCHAND <amarchan@e1r7p22.clusters.42paris.fr>
2022-07-18 13:47:09 +0200

    FIX - added line *err = 0
    BODY:
    withtout this line, the next time the user does a syntax error, because *err
    already equals something different than 0, program didn't change *err value
    
    TODO:
    N/A
    
    	modified:   srcs/lexer/pre_lexer.c

--- srcs/lexer/pre_lexer.c
+++ srcs/lexer/pre_lexer.c
@@ -6,16 +6,29 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 13:52:57 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/13 12:41:53 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/18 13:43:39 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	lex_space(char *str, int *err)
+{
+	if (ft_isspace(str[0]) && (str[1] == '|'
+		|| ft_isspace(str[ft_strlen(str) - 2]) && str[ft_strlen(str - 1)]))
+		{
+			*err = MISPLACED_PIPE;
+			printf("smbash: syntax error. Please check pipes.\n");
+			return (*err);
+		}
+	return (0);
+}
+
 int	pre_lexer(char *str, int *err)
 {
+	*err = 0;
 	if (lex_quote(str, err) || lex_pipe(str, err) || lex_redir(str, err)
-		|| lex_sym(str, err) || lex_brackets(str, err))
+		|| lex_sym(str, err) || lex_brackets(str, err) || lex_space(str, err))
 		return ((*err));
 	return (0);
 }
