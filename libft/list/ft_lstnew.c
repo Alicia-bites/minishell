049b3a526e4d423481c3ca7a02efb572f31b424d
Anthony BARRIER <abarrier@e1r7p20.clusters.42paris.fr>
2022-07-18 15:52:25 +0200

    NORM - libft ok
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   include/ft_list.h
    	modified:   list/ft_lstnew.c

--- libft/list/ft_lstnew.c
+++ libft/list/ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 09:23:22 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/18 11:42:44 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/18 15:40:33 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-t_list	*ft_lstnew(char *str, int i, e_toktype toktype)
+t_list	*ft_lstnew(char *str, int i, t_toktype toktype)
 {
 	t_list	*elt;
 
