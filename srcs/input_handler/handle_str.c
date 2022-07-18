f933ca15e5f3e308668542f051798aafb2f9b34d
Alicia MARCHAND <amarchan@e1r7p22.clusters.42paris.fr>
2022-07-18 15:11:32 +0200

    RM - comment in function
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/input_handler/handle_str.c

--- srcs/input_handler/handle_str.c
+++ srcs/input_handler/handle_str.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:24:51 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/13 16:25:04 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/18 13:43:29 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	handle_str(char **str, t_list **token_list, int *err)
 		print_lst(*token_list);
 		// execute_command(token_list);
 		ft_lstclear(token_list);
+		// printf("str = %s\n", *str);
 		if (is_not_empty(*str))
 			add_history(*str);
 }
