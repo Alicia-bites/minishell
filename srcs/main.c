d5e6eaf6c32849db8a6e24734083d537bd863120
Anthony BARRIER <abarrier@e1r7p20.clusters.42paris.fr>
2022-07-18 15:54:44 +0200

    UPDT - usage as Alicia branch, Antho's test zone commented
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/18 15:29:50 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/18 15:49:20 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,22 +83,23 @@ int	main(int argc, char **argv, char **envp)
 	int	err;
 
 	err = 0;
- 	t_ulist	**env_lst;
-
-	if (ft_set_sigaction() == -1)
-		printf("Setting up sigaction failed.\n");
- 	env_lst = ft_lst_init();
- 	if (!env_lst)
- 		return (1);
- 	if (env_lst_set(envp, env_lst))
- 	{
- 		ft_lst_free(env_lst, &env_free);
- 		return (2);
- 	}
- 	env_lst_show(env_lst);
-	antho_test(env_lst);
- 	env_lst_show(env_lst);
-	get_input(env_lst);
- 	ft_lst_free(env_lst, &env_free);
+	get_input(&err, NULL);
+// 	t_ulist	**env_lst;
+//
+//	if (ft_set_sigaction() == -1)
+//		printf("Setting up sigaction failed.\n");
+// 	env_lst = ft_lst_init();
+// 	if (!env_lst)
+// 		return (1);
+// 	if (env_lst_set(envp, env_lst))
+// 	{
+// 		ft_lst_free(env_lst, &env_free);
+// 		return (2);
+// 	}
+// 	env_lst_show(env_lst);
+//	antho_test(env_lst);
+// 	env_lst_show(env_lst);
+//	get_input(&err, env_lst);
+// 	ft_lst_free(env_lst, &env_free);
 	return (0);
 }
