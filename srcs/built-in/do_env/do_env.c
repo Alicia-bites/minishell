0c78e9b1f5b78d0e16a25df2a3ca86af4c732924
Anthony BARRIER <abarrier@e1r7p20.clusters.42paris.fr>
2022-07-18 09:03:39 +0200

    UPDT - add error message acording to argument number and remove old commented lines
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   do_env.c

--- srcs/built-in/do_env/do_env.c
+++ srcs/built-in/do_env/do_env.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:22:05 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/14 17:15:55 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/18 08:02:11 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int	do_env(t_ulist **envp, t_cmd *cmd)
 	if (!cmd)
 		return (1);
 	if (ft_strstrlen(cmd->fullcmd) == 0 || ft_strstrlen(cmd->fullcmd) > 1)
-		return (2);
+	{
+		ft_panic(EINVAL, NULL);
+		return (1);
+	}
 	else if (ft_strstrlen(cmd->fullcmd) == 1) //case: env
 		ft_lst_func_lst(envp, &do_env_show);
-//	else if (ft_strstrlen(cmd->fullcmd) >= 2 && ft_strchr(cmd->arg, ENV_SEP)) //case: env NAME1=value
-//	{
-//		do_env_update_lst(envp, &cmd->fullcmd[1]);
-//		ft_lst_func_lst(envp, &do_env_show);
-//	}
 	return (0);
 }
