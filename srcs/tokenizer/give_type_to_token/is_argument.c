1ca119821481c079c81faf77ad573e0ad6a4af5e
alicia <alicia.mrchd@gmail.com>
2022-07-19 14:19:44 +0200

    ADD - new function following_file_or_sep
    BODY:
    Check if tokenb is behind heresep or file
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/give_type_to_token/is_argument.c

--- srcs/tokenizer/give_type_to_token/is_argument.c
+++ srcs/tokenizer/give_type_to_token/is_argument.c
@@ -6,17 +6,31 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 09:47:14 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/18 18:22:34 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/19 13:51:20 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "minishell.h"
+
+int	following_file_or_sep(t_list **token_list)
+{
+	if ((*token_list)->index >= 1)
+	{
+		if ((*token_list)->prev->toktype == TOK_FILE
+			|| (*token_list)->prev->toktype == TOK_HERESEP)
+			return (1);
+	}
+	return (0);
+}
 
 // Is argument a token that is a $, or preceded by a space and then
 // a built-in or a command
 int	is_argument(t_list **token_list)
 {
-	if ((*token_list)->index >= 2)
+	extern t_global	global;
+	
+	if ((*token_list)->index >= 1)
 	{
 		if ((*token_list)->prev->toktype == TOK_BUILTIN
 			&& (ft_strcmp((*token_list)->token, "$"))
@@ -27,6 +41,10 @@ int	is_argument(t_list **token_list)
 		(*token_list)->toktype = TOK_ARG;
 	else if (is_combo_heredoc(token_list))
 		(*token_list)->toktype = TOK_ARG;
+	if (following_file_or_sep(token_list) && global.seen_tok_cmd)
+		(*token_list)->toktype = TOK_ARG;
+	else if (following_file_or_sep(token_list))
+		(*token_list)->toktype = TOK_CMD;
 	if ((*token_list)->toktype == TOK_ARG)
 	{
 		while (*token_list && !is_operator((*token_list)->token))
