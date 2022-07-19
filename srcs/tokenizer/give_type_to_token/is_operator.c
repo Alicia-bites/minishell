ab2c7596f89fffc4cff39043136b5d02bcbc6575
alicia <alicia.mrchd@gmail.com>
2022-07-19 16:38:05 +0200

    ADD - condition to see if operator is out of an expansion or not
    BODY:
    check if global.ignore_op is set to 1. if so, operator is not an operator so we return 0, and we set global.ignore_op back to 0
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/give_type_to_token/is_operator.c

--- srcs/tokenizer/give_type_to_token/is_operator.c
+++ srcs/tokenizer/give_type_to_token/is_operator.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:58:50 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/19 14:25:27 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/19 16:36:40 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ e_toktype	is_operator(char *str)
 {
 	extern t_global	global;
 
-	
 	if (str)
 	{
+		if (global.ignore_op == 1)
+		{
+			global.ignore_op = 0;
+			return (0);			
+		}
 		if (ft_strstr(str, "|"))
 		{
 			global.seen_tok_cmd = 0;
