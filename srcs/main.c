4741901ce2eeced68b0e1a2c2adb94a7268fb5f7
Anthony BARRIER <abarrier@e1r7p22.clusters.42paris.fr>
2022-07-20 10:43:46 +0200

    TESTER - Modify antho_test area to active and test easily built-in function
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:17:17 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/19 15:05:00 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/20 10:00:00 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 static void	antho_test(t_ulist **env_lst)
 {
+	int	case_cd;
+	int	case_env;
+	int	case_export;
+	int	case_unset;
+
+	case_cd = 0;
+	case_env = 0;
+	case_export = 1;
+	case_unset = 0;
+
  	t_cmd	test_env_cmd;
 // //	char test_env_cmd_arg[] = "env";
 // //	char *test_env_cmd_fullcmd[] = { "env", NULL };
@@ -28,25 +38,33 @@ static void	antho_test(t_ulist **env_lst)
  	test_env_cmd.arg = test_env_cmd_arg;
  	test_env_cmd.fullcmd = test_env_cmd_fullcmd;
  	test_env_cmd.fullpath = test_env_cmd_path;
- 	do_env(env_lst, &test_env_cmd);
-	ft_lst_func_lst(env_lst, &do_export_show);
-	ft_lst_func_lst(env_lst, &do_env_show);
- 	printf("%s\n", SEP_P);
+	if (case_env)
+	{
+		do_env(env_lst, &test_env_cmd);
+		ft_lst_func_lst(env_lst, &do_export_show);
+		ft_lst_func_lst(env_lst, &do_env_show);
+		printf("%s\n", SEP_P);
+	}
 
  	t_cmd	test_export_cmd;
 // 	char test_export_cmd_arg[] = "export";
 // 	char *test_export_cmd_fullcmd[] = { "export", NULL };
-	char test_export_cmd_arg[] = "export ANTHO ANTHO=toto=titi=tata ANTHO ALICIA=BG @ELON ANTHO=";
-	char *test_export_cmd_fullcmd[] = { "export", "ANTHO", "ANTHO=toto=titi=tata", "ANTHO", "ALICIA=BG", "@ELON", "ANTHO=", NULL };
- 	char test_export_cmd_path[] = "built-in";
+	char test_export_cmd_arg[] = "export ANTHO={echo -n}";
+	char *test_export_cmd_fullcmd[] = { "export", "ANTHO", "{echo -n}", NULL };
+// 	char test_export_cmd_arg[] = "export ANTHO ANTHO=toto=titi=tata ANTHO ALICIA=BG @ELON ANTHO=";
+//	char *test_export_cmd_fullcmd[] = { "export", "ANTHO", "ANTHO=toto=titi=tata", "ANTHO", "ALICIA=BG", "@ELON", "ANTHO=", NULL };
+	char test_export_cmd_path[] = "built-in";
 
  	test_export_cmd.arg = test_export_cmd_arg;
  	test_export_cmd.fullcmd = test_export_cmd_fullcmd;
  	test_export_cmd.fullpath = test_export_cmd_path;
-	do_export(env_lst, &test_export_cmd);
-	ft_lst_func_lst(env_lst, &do_export_show);
-	ft_lst_func_lst(env_lst, &do_env_show);
- 	printf("%s\n", SEP_P);
+	if (case_export)
+	{
+		do_export(env_lst, &test_export_cmd);
+		ft_lst_func_lst(env_lst, &do_export_show);
+		ft_lst_func_lst(env_lst, &do_env_show);
+		printf("%s\n", SEP_P);
+	}
 
  	t_cmd	test_unset_cmd;
 // 	char test_unset_cmd_arg[] = "unset";
@@ -58,10 +76,13 @@ static void	antho_test(t_ulist **env_lst)
  	test_unset_cmd.arg = test_unset_cmd_arg;
  	test_unset_cmd.fullcmd = test_unset_cmd_fullcmd;
  	test_unset_cmd.fullpath = test_unset_cmd_path;
- 	do_unset(env_lst, &test_unset_cmd);
- 	ft_lst_func_lst(env_lst, &do_export_show);
-	ft_lst_func_lst(env_lst, &do_env_show);
-	printf("%s\n", SEP_P);
+	if (case_unset)
+	{
+		do_unset(env_lst, &test_unset_cmd);
+		ft_lst_func_lst(env_lst, &do_export_show);
+		ft_lst_func_lst(env_lst, &do_env_show);
+		printf("%s\n", SEP_P);
+	}
 
  	t_cmd	test_cd_cmd;
 // 	char test_cd_cmd_arg[] = "cd";
@@ -75,10 +96,13 @@ static void	antho_test(t_ulist **env_lst)
 	test_cd_cmd.arg = test_cd_cmd_arg;
  	test_cd_cmd.fullcmd = test_cd_cmd_fullcmd;
  	test_cd_cmd.fullpath = test_cd_cmd_path;
- 	do_cd(env_lst, &test_cd_cmd);
- 	ft_lst_func_lst(env_lst, &do_export_show);
-	ft_lst_func_lst(env_lst, &do_env_show);
-	printf("%s\n", SEP_P);
+	if (case_cd)
+	{
+		do_cd(env_lst, &test_cd_cmd);
+		ft_lst_func_lst(env_lst, &do_export_show);
+		ft_lst_func_lst(env_lst, &do_env_show);
+		printf("%s\n", SEP_P);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
