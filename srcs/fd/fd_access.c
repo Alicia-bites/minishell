b3ffa23e10606aaa76743949faf560d617408fdf
Anthony BARRIER <abarrier@e1r7p22.clusters.42paris.fr>
2022-07-20 10:38:18 +0200

    UPDT - use ft_shell_msg function
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/fd/fd_access.c

--- srcs/fd/fd_access.c
+++ srcs/fd/fd_access.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 16:17:48 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/19 16:23:38 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/20 10:11:20 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ int	fd_access(char *fd, int mode)
 {
 	if (access(fd, F_OK) != 0)
 	{
-//		ft_shell_msg(ENOENT, fd);
+		ft_shell_msg(ENOENT, fd);
 		return (8);
 	}
 	else if (mode == ACCESS_X && access(fd, X_OK) != 0)
 	{
-//		ft_shell_msg(EACCES, fd);
+		ft_shell_msg(EACCES, fd);
 		return (1);
 	}
 	else if (mode == ACCESS_W && access(fd, W_OK) != 0)
 	{
-//		ft_shell_msg(EACCES, fd);
+		ft_shell_msg(EACCES, fd);
 		return (2);
 	}
 	else if (mode == ACCESS_R && access(fd, R_OK) != 0)
 	{
-//		ft_shell_msg(EACCES, fd);
+		ft_shell_msg(EACCES, fd);
 		return (4);
 	}
 	return (0);
