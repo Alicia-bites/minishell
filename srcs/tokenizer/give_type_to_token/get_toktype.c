bb7244b4f5169795a922d1321d620dbc7d1d3f69
alicia <alicia.mrchd@gmail.com>
2022-07-21 19:03:21 +0200

    ADD - comment one line that looks useless, testing
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/give_type_to_token/get_toktype.c

--- srcs/tokenizer/give_type_to_token/get_toktype.c
+++ srcs/tokenizer/give_type_to_token/get_toktype.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:58:10 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/21 18:42:14 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/21 18:58:58 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ void	get_toktype(t_list **token_list)
 			(*token_list)->toktype = is_operator((*token_list)->token);
 		else if (!only_space_in_str((*token_list)->token))
 			(*token_list)->toktype = TOK_SPACE;
-		else if (!only_space_in_str((*token_list)->token))
-			(*token_list)->toktype = TOK_CMD;
+		// else if (!only_space_in_str((*token_list)->token))
+		// 	(*token_list)->toktype = TOK_CMD;
 		else if (is_filename(token_list))
 			(*token_list)->toktype = TOK_FILE;
 		else if (is_heredoc_sep(token_list))
