9502e6fcb31e75acd9d7caee0ae5937aaf633788
Anthony BARRIER <abarrier@e1r3p1.clusters.42paris.fr>
2022-07-22 16:55:51 +0200

    FIX - ensure to execute a builtin or a classical command if only one command is present in the readline
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/cmd/cmd_execution.c

--- srcs/cmd/cmd_execution.c
+++ srcs/cmd/cmd_execution.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:02:20 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/22 16:10:40 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/22 16:54:56 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int	cmd_execution(t_ulist **cmd_lst, int n_cmd)
 		cmd = (t_cmd *)(*cmd_lst)->content;
 		if (cmd->toktype == TOK_BUILTIN)
 			res = do_builtin(cmd_lst, cmd);
+		else
+			res = pipe_run(cmd_lst, n_cmd);
 	}
 	else
 		res = pipe_run(cmd_lst, n_cmd);
