6c8c0efe1b412bf2b5be28f3cdc3aac586e2adf9
Anthony BARRIER <abarrier@e1r3p1.clusters.42paris.fr>
2022-07-22 14:42:56 +0200

    UPDT - close evert filedescriptor for each command in the fork
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   pipe_close_pfd.c

--- srcs/pipe/pipe_close_pfd.c
+++ srcs/pipe/pipe_close_pfd.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:30:05 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/21 19:30:40 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/22 13:27:48 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ void	pipe_close_pfd(void *content)
 	cmd = (t_cmd *)content;
 	if (!cmd)
 		return ;
+	if (cmd->fd_r >= 0)
+		close(cmd->fd_r);
+	if (cmd->fd_w >= 0)
+		close(cmd->fd_w);
 	if (cmd->pfd_r >= 0)
 		close(cmd->pfd_r);
 	if (cmd->pfd_w >= 0)
