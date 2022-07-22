7bc4025128f4b8a3b20d6115aa1875c8569d647d
alicia <alicia.mrchd@gmail.com>
2022-07-22 16:43:54 +0200

    RM - special case for spaces (no need anymore because we get rid of them)
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/print_lst.c

--- srcs/parsing/print_lst.c
+++ srcs/parsing/print_lst.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:11:16 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/28 15:43:47 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/22 16:25:43 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ void	print_lst(t_list *lst)
 	while (iterator)
 	{
 		printf("index = %d\n", iterator->index);
-		if (ft_isspace(iterator->token[0]))
-		{
-			i = 0;
-			while (iterator->token[i])
-				printf("%d ", iterator->token[i++]);
-			printf("\n");
-		}
-		else
-			printf("token = %s\n", iterator->token);
+		// if (ft_isspace(iterator->token[0]))
+		// {
+		// 	i = 0;
+		// 	while (iterator->token[i])
+		// 		printf("%d ", iterator->token[i++]);
+		// 	printf("\n");
+		// }
+		// else
+		printf("token = %s\n", iterator->token);
 		printf("toktype = %u\n", iterator->toktype);
 		// printf("prev = %p\n", iterator->prev);
 		// printf("next = %p\n", iterator->next);
