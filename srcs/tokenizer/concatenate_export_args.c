bb4a6925eba4fe7936e3a2ada07e53a05942c854
Alicia MARCHAND <amarchan@e1r12p6.clusters.42paris.fr>
2022-07-26 11:59:09 +0200

    ADD - function to concatenate echo args, maybe won't be necessary - commented  for now.
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/concatenate_export_args.c

--- srcs/tokenizer/concatenate_export_args.c
+++ srcs/tokenizer/concatenate_export_args.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 17:47:33 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/25 18:09:47 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/26 11:58:59 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,40 @@ void	concatenate_export_args(t_list **token_list)
 	tmp = *token_list;
 	while (*token_list)
 	{
-		if (!ft_strcmp((*token_list)->token, "export")
-			|| !ft_strcmp((*token_list)->token, "echo")
-			|| !ft_strcmp((*token_list)->token, "echo -n"))
+		if (!ft_strcmp((*token_list)->token, "export"))
 		{
 			if (!(*token_list)->next)
 				return ;
 			else
 				*token_list = (*token_list)->next;
-			if ((!ft_strcmp((*token_list)->prev->token, "echo")
-					|| !ft_strcmp((*token_list)->prev->token, "echo -n"))
-				|| (!ft_strcmp((*token_list)->prev->token, "export")
-					&& seen_equal_last((*token_list)->token)))
-						built_new_token_list(token_list);
+			if (seen_equal_last((*token_list)->token))
+				built_new_token_list(token_list);
 		}
 		if (*token_list)
 			*token_list = (*token_list)->next;
 	}
 	*token_list = tmp;
 }
+
+// void	concatenate_echo_args(t_list **token_list)
+// {
+// 	t_list	*tmp;
+// 	int		i;
+
+// 	tmp = *token_list;
+// 	while (*token_list)
+// 	{
+// 		if (!ft_strcmp((*token_list)->token, "echo")
+// 			|| !ft_strcmp((*token_list)->token, "echo -n"))
+// 		{
+// 			if (!(*token_list)->next)
+// 				return ;
+// 			else
+// 				*token_list = (*token_list)->next;
+// 			built_new_token_list(token_list);
+// 		}
+// 		if (*token_list)
+// 			*token_list = (*token_list)->next;
+// 	}
+// 	*token_list = tmp;
+// }
