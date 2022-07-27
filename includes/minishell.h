00e50c1ff6d86f6f33661fc4a7577456844b4e76
alicia <alicia.mrchd@gmail.com>
2022-07-27 17:40:44 +0200

    ADD - count_quotes in lexer in header file so I can use it in trim_quotes.c
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   includes/minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 10:28:47 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/27 12:39:01 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/27 15:51:05 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -186,6 +186,8 @@ int					count_single(char *str);
 //lexer
 int					lex_pipe(char *str, int *err);
 int					lex_quote(char *str, int *err);
+int					count_double_for_lexer(char *str);
+int					count_single_for_lexer(char *str);
 int					lex_redir(char *str, int *err);
 int					lex_sym(char *str, int *err);
 int					lex_brackets(char *str, int *err);
