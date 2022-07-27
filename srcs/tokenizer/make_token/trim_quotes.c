678c77996c5448597db28a42ad45319a2c3dea99
alicia <alicia.mrchd@gmail.com>
2022-07-27 17:39:56 +0200

    ADD - trimming quote perfectly
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/make_token/trim_quotes.c

--- srcs/tokenizer/make_token/trim_quotes.c
+++ srcs/tokenizer/make_token/trim_quotes.c
@@ -6,108 +6,67 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 17:22:18 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/26 17:52:22 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/27 17:39:32 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*built_output_d_quote(char *str, int count_quotes)
+int	get_malloc_size(char *str)
 {
-	int		i;
-	int		j;
-	char	*output;
+	int	len;
 
-	output = malloc(sizeof(char) * (ft_strlen(str) - count_quotes + 1));
-	if (!output)
-	{
-		ft_panic(MALLOC_FAILURE, __FILE__, NULL);
-		return (NULL);
-	}
-	i = 0;
-	j = 0;
-	while (i <= (ft_strlen(str) - count_quotes))
-	{
-		if (str[j] != '\"')
-			output[i++] = str[j];
-		j++;
-	}
-	output[i] = '\0';
-	return (output);
-}
-
-static char	*trim_double_quotes(char *str)
-{
-	int		i;
-	char	*output;
-	int		count_quotes;
-	
-	i= 0;
-	count_quotes = 0;
-	output = NULL;
-	while (str[i])
-	{
-		if (str[i] == '\"')
-			count_quotes++;
-		i++;
-	}
-	output = built_output_d_quote(str, count_quotes);
-	return (output);
-}
-
-static char	*built_output_s_quote(char *str, int count_quotes)
-{
-	int		i;
-	int		j;
-	char	*output;
-
-	output = malloc(sizeof(char) * (ft_strlen(str) - count_quotes + 1));
-	if (!output)
-	{
-		ft_panic(MALLOC_FAILURE, __FILE__, NULL);
-		return (NULL);
-	}
-	i = 0;
-	j = 0;
-	while (i <= (ft_strlen(str) - count_quotes))
-	{
-		if (str[j] != '\'')
-			output[i++] = str[j];
-		j++;
-	}
-	output[i] = '\0';
-	return (output);
-}
-
-static char	*trim_single_quotes(char *str)
-{
-	int		i;
-	char	*output;
-	int		count_quotes;
-	
-	i= 0;
-	output = NULL;
-	count_quotes = 0;
-	while (str[i])
-	{
-		if (str[i] == '\'')
-			count_quotes++;
-		i++;
-	}
-	output = built_output_s_quote(str, count_quotes);
-	printf("output = %s\n", output);
-	return (output);
+	len = count_double_for_lexer(str) + count_single_for_lexer(str);
+	return (len);	
 }
 
 char *trim_quotes(char *str)
 {
 	char	*output;
+	int		s;
+	int		d;
+	int		j;
+	int		i;
+	int		len;
 	
-	if (str[0] == '\"')
-		output = trim_double_quotes(str);
-	else if (str[0] == '\'')
-		output = trim_single_quotes(str);
-	else if (str[0] != '\'' && str[0] != '\"')
+	len = 0;
+	len = ft_strlen(str) - get_malloc_size(str);
+	if (len == ft_strlen(str))
 		return (str);
+	output = malloc(sizeof(char) * len + 1);
+	if (!output)
+	{
+		ft_panic(MALLOC_FAILURE, __FILE__, NULL);
+		return (NULL);
+	}
+	s = 0;
+	d = 0;
+	j = 0;
+	i = 0;
+	while (i <= len)
+	{
+		while (str[j] == '\'' && (!d))
+		{
+			s++;
+			j++;			
+		}
+		while (str[j] == '\"' && (!s))
+		{
+			d++;
+			j++;			
+		}
+		if (str[j] == '\'' && !(d % 2))
+		{
+			j++;
+			s++;			
+		}
+		else if (str[j] == '\"' && !(s % 2))
+		{
+			j++;
+			d++;
+		}
+		output[i++] = str[j++];
+	}
+	output[i] = '\0';
 	return (output);
 }
