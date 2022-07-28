3a78a3c5c59ad52a5665dc93f02574c804a9ef8e
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-07-28 11:50:49 +0200

    FIX - added condition to make sure *end is always smaller than input_list length and added condition to not built token if token as already been built in is_quote
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/make_token/is_word.c

--- srcs/tokenizer/make_token/is_word.c
+++ srcs/tokenizer/make_token/is_word.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 18:10:26 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/27 19:13:35 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/28 10:57:51 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	is_word(t_chartype *input_list, int *start, int *end,
 	quote = 0;
 	if (input_list[*end].type == CH_WORD)
 	{
-		while (input_list[*end].type == CH_WORD)
+		while (*end < input_list->length
+			&& input_list[*end].type == CH_WORD)
 		{
 			if (echo_n(input_list, end, &quote))
 			{
@@ -37,11 +38,17 @@ void	is_word(t_chartype *input_list, int *start, int *end,
 					*start = *end;
 				return ;
 			}
+			if (*end + 1 > input_list->length)
+				break ;
 			(*end)++;
 		}
-		if (!empty_string(input_list, *end))
-			is_quote(input_list, start, end, token_list);			
-		built_token(input_list, *start, *end, token_list);
-		*start = *end;
+		quote = *end;
+		if (!empty_string(input_list, *end) && *end + 1 < input_list->length)
+			is_quote(input_list, start, end, token_list);		
+		if (quote == *end)
+		{
+			built_token(input_list, *start, *end, token_list);
+			*start = *end;
+		}
 	}
 }
