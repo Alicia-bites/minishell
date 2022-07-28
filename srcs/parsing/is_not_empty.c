a575226e4083958f6436748e4aa1ae1a69e6257c
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-07-28 11:47:02 +0200

    RM - puts hey you
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/is_not_empty.c

--- srcs/parsing/is_not_empty.c
+++ srcs/parsing/is_not_empty.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 11:13:11 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/23 11:11:05 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/28 10:58:53 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@ int	is_not_empty(char *str)
 	if (str == 0 || !ft_strcmp(" ", str) || !ft_strcmp("\f", str)
 		|| !ft_strcmp("\n", str) || !ft_strcmp("\r", str)
 		|| !ft_strcmp("\t", str) || !ft_strcmp("\v", str))
-	{
-		puts("hey you");
 		return (0);
-	}
 	return (1);
 }
