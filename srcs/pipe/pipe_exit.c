8aac9c3a5e3dcdafc679fc86632758a74ccd380b
BarrierAntho <barrier.antho@gmail.com>
2022-07-28 08:53:42 +0200

    UPDT - change the prototype of pipe_exit to handle and return the errno
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         ../../includes/minishell.h
    	modifié :         pipe_exit.c

--- srcs/pipe/pipe_exit.c
+++ srcs/pipe/pipe_exit.c
@@ -6,17 +6,17 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 12:53:38 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/22 13:30:23 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/27 13:50:27 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	pipe_exit(t_ulist **cmd_lst, t_cmd *cmd)
+void	pipe_exit(t_ulist **cmd_lst, t_cmd *cmd, int err_no)
 {
 	rl_clear_history();
 	ft_lstclear(cmd->tok_lst);
 	ft_lst_free(cmd->env_lst, &env_free);
 	ft_lst_free(cmd_lst, &cmd_free);
-	exit(1);
+	exit(err_no);
 }
