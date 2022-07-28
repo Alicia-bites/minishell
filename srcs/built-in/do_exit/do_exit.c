8cda4ce733aef8f0a8db27ceeb48e02b42e70c2e
BarrierAntho <barrier.antho@gmail.com>
2022-07-28 09:01:39 +0200

    UPDT - return the exit value of the command itself if there is an argument or return the value of the last executed command
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         do_exit.c

--- srcs/built-in/do_exit/do_exit.c
+++ srcs/built-in/do_exit/do_exit.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 18:22:30 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/27 11:11:25 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/27 19:52:02 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	do_exit(t_ulist **envp, t_ulist **cmd_lst, t_cmd *cmd)
 {
-	int			internal_error;
-	long long	exit_return;
+	int				internal_error;
+	long long		exit_return;
+	extern t_global	global;
 
+	printf("exit\n");
 	internal_error = 0;
 	exit_return = 0;
-	if (cmd->n_arg > 1)
+	if (cmd->n_arg == 1)
+		exit_return = global.exit_status;
+	else if (cmd->n_arg > 1)
 		internal_error = ft_str_isll(cmd->fullcmd[1]);
 	if (cmd->n_arg == 2 && internal_error == 0)
 		exit_return = ft_atoll(cmd->fullcmd[1]);
@@ -28,10 +32,8 @@ int	do_exit(t_ulist **envp, t_ulist **cmd_lst, t_cmd *cmd)
 	ft_lst_free(envp, &env_free);
 	ft_lst_free(cmd_lst, &cmd_free);
 	if (internal_error > 0)
-	{
-		ft_panic(EINVAL, __FILE__, NULL);
-		exit_return = EINVAL;
-	}
+		exit_return = ft_panic_value(EINVAL, __FILE__, NULL, EINVAL);
+	global.exit_status = exit_return;
 	exit(exit_return);
 	return (exit_return);
 }
