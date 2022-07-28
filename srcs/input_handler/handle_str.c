903d85a067618cada2c5a763601cda4515db9cc7
BarrierAntho <barrier.antho@gmail.com>
2022-07-28 08:41:07 +0200

    UPDT - add protection to make sure to have token listm cmd list
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         handle_str.c

--- srcs/input_handler/handle_str.c
+++ srcs/input_handler/handle_str.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:24:51 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/25 16:43:18 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/27 18:58:01 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,32 @@ void	handle_str(char **str, t_list **token_list, int *err, t_ulist **envp)
 
 	cmd_list = NULL;
 	cmd_list = ft_lst_init();
+//	cmd_list = NULL;;
 	if (!cmd_list)
 	{
 		ft_lstclear(token_list);
 		return ;
 	}
-	ft_parse(*str, token_list, err, *envp);
-	print_lst(*token_list);
-	cmd_create_lst(token_list, envp, cmd_list);
+	if(ft_parse(*str, token_list, err, *envp))
+	{
+		ft_lstclear(token_list);
+		ft_lst_free(cmd_list, &cmd_free);
+		return ;
+	}
+//	print_lst(*token_list);
+	if(!*token_list)
+	{
+		ft_panic(-1, __FILE__, ERR_NOTOK);
+		ft_lstclear(token_list);
+		ft_lst_free(cmd_list, &cmd_free);
+		return ;
+	}
+	if(cmd_create_lst(token_list, envp, cmd_list))
+	{
+		ft_lstclear(token_list);
+		ft_lst_free(cmd_list, &cmd_free);
+		return ;
+	}
 	fd_link_in_out_file(token_list, cmd_list, DIR_IN);
 	fd_pipe(cmd_list);
 	//ft_lst_func_lst(cmd_list, &cmd_show);
