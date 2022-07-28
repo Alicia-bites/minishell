e4b1177139a5a74f8f1e99629e59f338b87feb39
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-07-28 21:38:47 +0200

    NOTHING TO DECLARE
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/input_handler/handle_str.c

--- srcs/input_handler/handle_str.c
+++ srcs/input_handler/handle_str.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:24:51 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/28 15:44:58 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/28 19:24:16 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	handle_str(char **str, t_list **token_list, int *err, t_ulist **envp)
 		return ;
 	}
 	ft_parse(*str, token_list, err, *envp);
-	print_lst(*token_list);
+	// print_lst(*token_list);
 	cmd_create_lst(token_list, envp, cmd_list);
 	fd_link_in_out_file(token_list, cmd_list, DIR_IN);
 	fd_pipe(cmd_list);
