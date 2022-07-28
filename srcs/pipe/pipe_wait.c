54592e0af1bae16c1db3b7a97a90c1a6517b4b48
Anthony BARRIER <abarrier@e1r4p1.clusters.42paris.fr>
2022-07-28 21:08:15 +0200

    UPDT - handle signal and return code 130 if CTR+C
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   pipe_wait.c

--- srcs/pipe/pipe_wait.c
+++ srcs/pipe/pipe_wait.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:23:34 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/28 08:46:07 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/28 20:54:26 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,27 @@
 int	pipe_wait(int n_cmd, int pid)
 {
 	int	status;
+	int	status_child;
 	int	i;
+	int	res;
 
 	status = 1;
+	status_child = 1;
 	i = 0;
+	res = 0;
 	waitpid(pid, &status, 0);
 	n_cmd--;
 	while (i < n_cmd)
 	{
-		waitpid(-1, NULL, 0);
+		waitpid(-1, &status_child, 0);
+		if (WIFSIGNALED(status_child) == 1)
+			res++;
 		i++;
 	}
-	return (WEXITSTATUS(status));
+	if (WIFSIGNALED(status) == 1)
+		res++;
+	if (res)
+		return (130);
+	else
+		return (WEXITSTATUS(status));
 }
