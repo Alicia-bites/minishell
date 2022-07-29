1cc21fe8a2d25c00398e9b52fa3082504aa6f114
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-07-29 15:09:30 +0200

    UPDT - work in progress
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/built-in/do_echo/do_echo.c

--- srcs/built-in/do_echo/do_echo.c
+++ srcs/built-in/do_echo/do_echo.c
@@ -6,29 +6,31 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 15:51:54 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/29 14:25:26 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/29 15:08:42 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	do_echo_remove_quotes(t_list **token_list)
+t_list	*do_echo_remove_quotes(t_cmd *cmd)
 {
 	t_list *save;
 
-	save = *token_list;
-	if ((*token_list)->next && 
-		!ft_strcmp((*token_list)->token, "echo") && (*token_list)->next->toktype == TOK_ARG)
+	save = *(cmd->tok_lst);
+	if ((*(cmd->tok_lst))->next && 
+		!ft_strcmp((*(cmd->tok_lst))->token, "echo") && (*(cmd->tok_lst))->next->toktype == TOK_ARG)
 	{
-		(*token_list) = (*token_list)->next;
-		while ((*token_list) && (*token_list)->toktype == TOK_ARG)
+		(*(cmd->tok_lst)) = (*(cmd->tok_lst))->next;
+		while ((*(cmd->tok_lst)) && (*(cmd->tok_lst))->toktype == TOK_ARG)
 		{
-			if (not_only_space_between_quotes((*token_list)->token))
-				trim_quotes((*token_list)->token);
-			(*token_list) = (*token_list)->next;
+			if (!not_only_space_between_quotes((*(cmd->tok_lst))->token))
+				(*(cmd->tok_lst))->token = trim_quotes((*(cmd->tok_lst))->token);
+			(*(cmd->tok_lst)) = (*(cmd->tok_lst))->next;
 		}
 	}
-	*token_list = save;
+	*cmd->tok_lst = save;
+	printf("token = %s\n", (*(cmd->tok_lst))->next->token);
+	return (save);
 }
 
 int	do_echo(t_ulist **envp, t_cmd *cmd)
@@ -41,8 +43,8 @@ int	do_echo(t_ulist **envp, t_cmd *cmd)
 	index = do_echo_get_index((*cmd->tok_lst));
 	if (index != 0)
 	{
-		do_echo_remove_quotes(cmd->tok_lst);
-		do_echo_n(envp, cmd, index);		
+		(*cmd->tok_lst) = do_echo_remove_quotes(cmd);
+		do_echo_n(envp, cmd, index);
 	}
 	if (!do_echo_valid_echo_n((*cmd->tok_lst)))
 		res = printf("\n");
