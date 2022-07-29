5851d683b02101b964a33c319d5034466ee27577
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-07-29 12:31:41 +0200

    ADD - index in do_echo_n
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/built-in/do_builtin.c

--- srcs/built-in/do_builtin.c
+++ srcs/built-in/do_builtin.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 14:56:18 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/28 12:22:17 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/29 10:57:08 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	do_builtin(t_ulist **cmd_lst, t_cmd *cmd)
 	else if (ft_strcmp(cmd->fullcmd[0], BUILT_ECHO) == 0)
 		return (do_echo(cmd->env_lst, cmd));
 	else if (ft_strcmp(cmd->fullcmd[0], BUILT_ECHO_N) == 0)
-		return (do_echo_n(cmd->env_lst, cmd));
+		return (do_echo_n(cmd->env_lst, cmd, 0));
 	else if (ft_strcmp(cmd->fullcmd[0], BUILT_ENV) == 0)
 		return (do_env(cmd->env_lst, cmd));
 	else if (ft_strcmp(cmd->fullcmd[0], BUILT_EXIT) == 0)
