9e101787ef43cfb38ef94dde1c60badaf98dc63c
Anthony BARRIER <abarrier@e1r4p1.clusters.42paris.fr>
2022-07-29 14:31:40 +0200

    UPDT - add TODO list to handle better signal return
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   pipe_run.c
    	modified:   pipe_wait.c

--- srcs/pipe/pipe_wait.c
+++ srcs/pipe/pipe_wait.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:23:34 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/28 20:54:26 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/07/29 12:05:31 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,25 @@ int	pipe_wait(int n_cmd, int pid)
 	int	i;
 	int	res;
 
-	status = 1;
-	status_child = 1;
+	status = 0;
+	status_child = 0;
 	i = 0;
 	res = 0;
 	waitpid(pid, &status, 0);
 	n_cmd--;
 	while (i < n_cmd)
 	{
+		status_child = 0;
 		waitpid(-1, &status_child, 0);
 		if (WIFSIGNALED(status_child) == 1)
 			res++;
 		i++;
 	}
+	// TODO
+	// Add hierarchy of SIGNAL MACRO
+	// Most of WAITPID SIGNAL handling
+	// IFEXITED
+	// IF....
 	if (WIFSIGNALED(status) == 1)
 		res++;
 	if (res)
