499e741ac4fed924d40582c3bcbc269c35cbef1c
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-07-29 12:32:07 +0200

    ADD - i starts at index
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/built-in/do_echo/do_echo_n.c

--- srcs/built-in/do_echo/do_echo_n.c
+++ srcs/built-in/do_echo/do_echo_n.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   do_echo.c                                          :+:      :+:    :+:   */
+/*   do_echo_n.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 09:56:41 by amarchan          #+#    #+#             */
-/*   Updated: 2022/06/28 11:25:17 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/07/29 10:24:57 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	do_echo_n(t_ulist **envp, t_cmd *cmd)
+int	do_echo_n(t_ulist **envp, t_cmd *cmd, int index)
 {
 	int	res;
 	int	i;
 
 	res = 0;
-	i = 1;
+	i = index;
 	if (cmd->n_arg == 1)
 		return (0);
 	while (i < (cmd->n_arg -1))
