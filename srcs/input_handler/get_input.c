a4d1436d6ea4709ab6dd2a73a2221218c46aeb5e
BarrierAntho <barrier.antho@gmail.com>
2022-08-02 17:31:24 +0200

    UPDT - stop using minishell read_line function and use readline directly
    BODY:
    Preparation to stop using t_global
    
    TODO:
    N/A
    
    	modifié :         get_input.c

--- srcs/input_handler/get_input.c
+++ srcs/input_handler/get_input.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:25:39 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/22 17:22:47 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/02 17:31:19 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	get_input(int *err, t_ulist **envp)
 	str = NULL;
 	while (42)
 	{
-		read_line(&str);
+		//read_line(&str);
+		str = readline("$: ");
 		if (!str)
 			exit_minishell(&token_list, err, envp);
 		if (str[0] != 0)
