ead2c50471183a998da555d775a6761d3fcab33c
BarrierAntho <barrier.antho@gmail.com>
2022-08-02 17:29:47 +0200

    UPDT - signal handling
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         cmd_execution.c

--- srcs/cmd/cmd_execution.c
+++ srcs/cmd/cmd_execution.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:02:20 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/27 20:20:21 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/02 11:45:54 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,21 @@ int	cmd_execution(t_ulist **cmd_lst, int n_cmd)
 
 	cmd = NULL;
 	res = 0;
+	sig_program_unset_action();
 	if (n_cmd == 1)
 	{
 		cmd = (t_cmd *)(*cmd_lst)->content;
 		if (cmd->toktype == TOK_BUILTIN)
+		{
+			sig_pipe_set_action();
 			res = do_builtin(cmd_lst, cmd);
+		}
 		else
 			res = pipe_run(cmd_lst, n_cmd);
 	}
 	else
 		res = pipe_run(cmd_lst, n_cmd);
+	sig_program_set_action();
 	global.exit_status = res;
 	return (res);
 }
