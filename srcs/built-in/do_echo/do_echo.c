980bbf3a0b920dbbda6ba9c96098404683a1786d
BarrierAntho <barrier.antho@gmail.com>
2022-08-10 16:17:19 +0200

    FIX - echo and echo -n with or without space
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         srcs/built-in/do_echo/do_echo.c
    	modifié :         srcs/built-in/do_echo/do_echo_n.c

--- srcs/built-in/do_echo/do_echo.c
+++ srcs/built-in/do_echo/do_echo.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 15:51:54 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/09 18:30:46 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/10 16:13:10 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ static int	only_double_quote_in_str(char *str)
 	return (0);
 }
 
+/*
 t_list	**do_echo_remove_quotes(t_cmd *cmd)
 {
 	t_list *save;
@@ -39,6 +40,28 @@ t_list	**do_echo_remove_quotes(t_cmd *cmd)
 	*cmd->tok_lst = save;
 	return (cmd->tok_lst);
 }
+*/
+
+t_list	*do_echo_remove_quotes(t_cmd *cmd)
+{
+	t_list *save;
+
+	save = cmd->tok_node;
+	if (cmd->tok_node->next && 
+		!ft_strcmp(cmd->tok_node->token, "echo") && (cmd->tok_node->next->toktype == TOK_ARG))
+	{
+		cmd->tok_node = cmd->tok_node->next;
+		while (cmd->tok_node && cmd->tok_node->toktype == TOK_ARG)
+		{
+			if (!not_only_space_between_quotes(cmd->tok_node->token)
+				&& !only_double_quote_in_str(cmd->tok_node->token))
+					cmd->tok_node->token = trim_quotes(cmd->tok_node->token);
+			cmd->tok_node = cmd->tok_node->next;
+		}
+	}
+	cmd->tok_node = save;
+	return (cmd->tok_node);
+}
 
 int	do_echo(t_ulist **envp, t_cmd *cmd)
 {
@@ -47,16 +70,22 @@ int	do_echo(t_ulist **envp, t_cmd *cmd)
 	
 	res = 0;
 	index = 1;
-	index = do_echo_get_index((*cmd->tok_lst));
+	//index = do_echo_get_index((*cmd->tok_lst));
+	//printf("cmd tok_node: %p\ttoken: %s\tindex: %d\n", cmd->tok_node, cmd->tok_node->token, cmd->tok_node->index);
+	index = do_echo_get_index((cmd->tok_node));
+	//printf("index: %d\n", index);
 	if (index != 0)
 	{
-		cmd->tok_lst = do_echo_remove_quotes(cmd);
+		//cmd->tok_lst = do_echo_remove_quotes(cmd);
+		cmd->tok_node = do_echo_remove_quotes(cmd);
 		ft_free_ptrptr_str(cmd->fullcmd);
-		cmd_init_prop_fullcmd(*cmd->tok_lst, cmd);
-		do_echo_n(envp, cmd, index);
+		//cmd_init_prop_fullcmd(*cmd->tok_lst, cmd);
+		cmd_init_prop_fullcmd(cmd->tok_node, cmd);
+		res += do_echo_n(envp, cmd, index);
 	}
-	if (!do_echo_valid_echo_n((*cmd->tok_lst)))
-		res = printf("\n");
+	//if (!do_echo_valid_echo_n((*cmd->tok_lst)))
+	if (!do_echo_valid_echo_n((cmd->tok_node)))
+		res += printf("\n");
 	if (res <= 0)
 		return (1);
 	else
