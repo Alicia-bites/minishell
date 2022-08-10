7b32a7c415f985cda2401d091332796e333161de
alicia <alicia.mrchd@gmail.com>
2022-08-10 15:13:47 +0200

    ADD - put everything I deleted in the last commit back - I need to check for quotes for only one operator is between quotes!
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/tokenizer/make_token/built_token.c

--- srcs/tokenizer/make_token/built_token.c
+++ srcs/tokenizer/make_token/built_token.c
@@ -6,29 +6,29 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:57:58 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/10 15:12:05 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/10 15:13:29 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// static int	check_quote(t_chartype *input_list, int start)
-// {
-// 	if (input_list[start].character == '\''
-// 		|| input_list[start].character == '\"')
-// 			return (1);
-// 	else
-// 	{
-// 		while (is_char_word(input_list[start].character))
-// 		{
-// 			start++;
-// 			if (input_list[start].character == '\''
-// 			|| input_list[start].character == '\"')
-// 				return (1);	
-// 		}
-// 	}
-// 	return (0);
-// }
+static int	check_quote(t_chartype *input_list, int start)
+{
+	if (input_list[start].character == '\''
+		|| input_list[start].character == '\"')
+			return (1);
+	else
+	{
+		while (is_char_word(input_list[start].character))
+		{
+			start++;
+			if (input_list[start].character == '\''
+			|| input_list[start].character == '\"')
+				return (1);	
+		}
+	}
+	return (0);
+}
 
 void	built_token(t_chartype *input_list, int start, int end,
 	t_list **token_list)
@@ -36,9 +36,9 @@ void	built_token(t_chartype *input_list, int start, int end,
 	int			k;
 	int			len;
 	char		*token;
-	// int			seen_quote;
+	int			seen_quote;
 
-	// seen_quote = check_quote(input_list, start);
+	seen_quote = check_quote(input_list, start);
 	len = end - start;
 	token = malloc(sizeof(char) * (len + 1));
 	if (!token)
@@ -53,6 +53,6 @@ void	built_token(t_chartype *input_list, int start, int end,
 	if (not_only_space_between_quotes(token))
 		token = trim_quotes_token(token, *token_list);
 	add_token_to_list(token, token_list);
-	// if (seen_quote && token != NULL)
-	// 	check_operator_presence(token, token_list);
+	if (seen_quote && token != NULL)
+		check_operator_presence(token, token_list);
 }
