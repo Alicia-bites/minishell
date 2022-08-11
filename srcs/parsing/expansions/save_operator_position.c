bc123608f8a7a62bb8b3cdc00dbd2e39b057f806
alicia <alicia.mrchd@gmail.com>
2022-08-11 12:40:00 +0200

    FIX - bad update of tab in global variable resulting in bug. Fixed.
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/save_operator_position.c

--- srcs/parsing/expansions/save_operator_position.c
+++ srcs/parsing/expansions/save_operator_position.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 12:02:19 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/21 13:47:29 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/11 12:36:02 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ static void	copy_pos(t_position position, int place, int n, char *expanded)
 	static int		save_pos = 0;
 	static int		ex_move_cursor = 0;
 
-	if (save_pos > position.pos)
+	if (save_pos >= position.pos)
 	{
 		ex_move_cursor = 0;
 		i = 0;
@@ -54,22 +54,37 @@ static void	copy_pos(t_position position, int place, int n, char *expanded)
 		ex_move_cursor = position.move_cursor;
 }
 
+static void	malloc_global_saved_pos(int n)
+{
+	extern t_global	global;
+
+	global.saved_pos = malloc(sizeof(int) * (n + 1));
+	if (!global.saved_pos)
+	{
+		ft_panic_null(-1, __FILE__, ERR_MALLOC);
+		return ;		
+	}
+	ft_bzero(global.saved_pos, n + 1);
+}
+
 void	save_operator_position(char *expanded, t_position position)
 {
 	int				i;
 	static int		n = 0;
 	extern t_global	global;
 	static int		save_pos = 0;
+	static int		n_dollars = 0;
 
-	if (save_pos > position.pos)
+	// printf("n_dollars = %d\n", n_dollars);
+	// printf("position.n_expansions = %d\n", position.n_expansions);
+	if (global.saved_pos && n_dollars == 0)
+		ft_bzero(global.saved_pos, n + 1);
+	if (save_pos >= position.pos)
 		n = 0;
 	save_pos = position.pos;
 	n += count_op(expanded);
-	if (!global.saved_pos)
-	{
-		global.saved_pos = malloc(sizeof(int) * (n + 1));
-		ft_bzero(global.saved_pos, n + 1);
-	}
+	if (!global.saved_pos || global.saved_pos[0] == '\0')
+		malloc_global_saved_pos(n);
 	i = 0;
 	while (expanded[i])
 	{
@@ -77,5 +92,9 @@ void	save_operator_position(char *expanded, t_position position)
 			copy_pos(position, i, n, expanded);
 		i++;
 	}
-	global.saved_pos[i++] = '\0';
+	global.saved_pos[n] = '\0';
+	n_dollars++;
+	if (n_dollars == position.n_expansions)
+		n_dollars = 0;		
+	print_global_saved_pos();
 }
