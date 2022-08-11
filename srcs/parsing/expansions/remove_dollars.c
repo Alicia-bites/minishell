66c6c2c62eaac2b3216e7b626e8245f15f26f051
alicia <alicia.mrchd@gmail.com>
2022-08-11 12:39:40 +0200

    ATOM - count_my_dollars
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/remove_dollars.c

--- srcs/parsing/expansions/remove_dollars.c
+++ srcs/parsing/expansions/remove_dollars.c
@@ -6,28 +6,11 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 14:48:01 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/09 14:48:59 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/11 11:04:33 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static int	count_my_dollars(char *str)
-{
-	int	i;
-	int n;
-	
-	i = 0;
-	n = 0;
-	while (i < ft_strlen(str))
-	{
-		if (i + 1 < ft_strlen(str) && str[i] == '$'
-			&& (str[i + 1] == '\'' || str[i + 1] == '"'))
-			n++;
-		i++;
-	}
-	return (n);
-}
 
 char	*remove_dollars( char *str)
 {
