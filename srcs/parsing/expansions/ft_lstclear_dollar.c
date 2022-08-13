4a45abb9074cd2835062debbf20eccd93ed16aba
alicia <alicia.mrchd@gmail.com>
2022-08-13 16:18:07 +0200

    FIX - wasn't freeing its content
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/ft_lstclear_dollar.c

--- srcs/parsing/expansions/ft_lstclear_dollar.c
+++ srcs/parsing/expansions/ft_lstclear_dollar.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 16:01:12 by amarchan          #+#    #+#             */
-/*   Updated: 2022/07/22 17:28:47 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/13 15:24:47 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	ft_lstclear_dollar(t_expanded **lst)
 	{
 		prev = iterator;
 		iterator = iterator->next;
+		if (prev->expanded)
+			free(prev->expanded);
 		free(prev);
 	}
 	*lst = NULL;
