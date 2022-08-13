0526a0bc92d17012572f65eeaebbe97e5b6b1994
alicia <alicia.mrchd@gmail.com>
2022-08-13 16:14:59 +0200

    FIX - leaks because ft_lsclear_dollar wasn't freeing its content.
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/expand_dollar.c

--- srcs/parsing/expansions/expand_dollar.c
+++ srcs/parsing/expansions/expand_dollar.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 19:47:06 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/12 14:33:52 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/13 16:13:38 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,13 @@ char	*expand_dollar(t_exp_arg exp_arg, int **tab)
 	full_size = 0;
 	expanded_list = NULL;
 	find_expansions(exp_arg, &expanded_list, &full_size);
-	exp_arg.str = remove_dollars(exp_arg.str);
+	if (ft_strstr(exp_arg.str, "$\'") || ft_strstr(exp_arg.str, "$\""))
+		exp_arg.str = remove_dollars(exp_arg.str);
 	if (!expanded_list)
-	{
 		return (exp_arg.str);
-	}
 	full_size += get_full_size(expanded_list) + ft_strlen(exp_arg.str);
 	new_str = insert_expansions(full_size, expanded_list, exp_arg.str);
 	*tab = save_operator_index(exp_arg.str, new_str, expanded_list);
 	ft_lstclear_dollar(&expanded_list);
-	ft_lstclear_back_dollar(&expanded_list);
 	return (new_str);
 }
