85a761f37039a60f4025985db330c19cfdc0ecc8
alicia <alicia.mrchd@gmail.com>
2022-08-13 16:18:29 +0200

    RM - useless stuff from when I was trying to save operator's position in expanded
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/handle_dollar_int.c

--- srcs/parsing/expansions/handle_dollar_int.c
+++ srcs/parsing/expansions/handle_dollar_int.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 09:42:30 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/10 12:22:09 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/13 15:06:33 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,9 @@ int	handle_dollar_int(t_exp_arg exp_arg, int i, t_expanded **expanded_list,
 	int			j;
 	char		*var;
 	char		*expanded;
-	t_position	position;
 	int			varsize;
 
 	expanded = handle_dollar_int_expanded();
-	position.move_cursor = ft_strlen(expanded) - (2 + 1);
-	position.pos = i;
 	*expanded_list = create_dollar_list(expanded, (*index)++);
 	varsize = 2;
 	return (varsize);
