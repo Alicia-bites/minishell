29090ee0e0a0a3b000249856749ded65363bd028
Anthony BARRIER <abarrier@e1r5p16.clusters.42paris.fr>
2022-08-15 14:09:08 +0200

    UPDT -function to be done to trim quotes in the tok->token
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   hd_write.c

--- srcs/heredoc/hd_write.c
+++ srcs/heredoc/hd_write.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 17:57:49 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/15 09:26:39 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/15 14:09:03 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,10 @@ void	hd_write(t_list *tok, t_cmd *cmd)
 	fullcmd[0] = HD_BIN_TRUE;
 	fullcmd[1] = NULL;
 	mode = hd_write_mode(tok);
+	if (mode == HD_NO_EXP) //IF tok-token contains quotes, no matter where
+	{
+		trim_quotes(tok->token);
+		printf("tok->token: %s\n", tok->token);
+	}
 	hd_write_str(tok, cmd, fullcmd, mode);
 }
