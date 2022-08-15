71dc006da29c8f6a793e88f1ca97fa8ff03afed0
Anthony BARRIER <abarrier@e1r5p16.clusters.42paris.fr>
2022-08-15 12:29:36 +0200

    UPDT - remove printf debug
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   hd_write_expansion.c
    	modified:   hd_write_str.c

--- srcs/heredoc/hd_write_expansion.c
+++ srcs/heredoc/hd_write_expansion.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 10:10:45 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/15 10:11:16 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/15 12:21:54 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,16 @@ char	*hd_write_expansion(char *str, t_ulist **envp)
 {
 	char		*output;
 	t_exp_arg	exp_arg;
-	
+
 	output = NULL;
-	printf("output 0: ptr = %p\n", output);
 	exp_arg.envp = *envp;
 	exp_arg.str = str;
 	if (ft_strstr(str, "$"))
 	{
-		printf("output 1-1: ptr = %p\n", output);
 		output = expand_dollar_heredoc(exp_arg);
-		//output = str;
-		printf("output 1-2: ptr = %p\n", output);
 		free(str);
 	}
 	else
-	{
 		output = str;
-		printf("output 2: ptr = %p\n", output);
-	}
-	printf("output 3: ptr = %p\n", output);
 	return (output);
 }
--- srcs/heredoc/hd_write_str.c
+++ srcs/heredoc/hd_write_str.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 17:57:49 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/15 10:03:07 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/15 11:13:33 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	hd_write_str(t_list *tok, t_cmd *cmd, char **fullcmd, int mode)
 	while (1)
 	{
 		str = readline(HD_PREFIX);
-		printf("str: %s\n", str);
 		if (!str || ft_strcmp(str, tok->token) == 0)
 		{
 			close(cmd->hd_r);
@@ -30,13 +29,8 @@ void	hd_write_str(t_list *tok, t_cmd *cmd, char **fullcmd, int mode)
 		}
 		else
 		{
-			printf("TEST\n");
 			if (mode == HD_EXP)
-			{
-				printf("TEST 2\n");
 				str = hd_write_expansion(str, cmd->env_lst);
-				printf("TEST 3: str ptr = %p\n", str);
-			}
 			ft_putendl_fd(str, cmd->hd_r);
 		}
 		free(str);
