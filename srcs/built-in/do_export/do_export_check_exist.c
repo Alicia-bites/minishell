5749685124fa6c41679aff222f10449b370d7724
Anthony BARRIER <abarrier@e1r5p16.clusters.42paris.fr>
2022-08-15 14:54:31 +0200

    FIX - fix export issue when several env var name have the same basename as the, there
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   do_export_check_exist.c

--- srcs/built-in/do_export/do_export_check_exist.c
+++ srcs/built-in/do_export/do_export_check_exist.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 10:17:50 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/12 15:15:29 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/15 14:42:21 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,16 @@ t_ulist	*do_export_check_exist(t_ulist **env_lst, char *str, int sep_pos)
 	while (obj)
 	{
 		env = (t_env *)obj->content;
-		if (sep_pos >= 0)
+		if (sep_pos > 0)
 		{
 			if (ft_strncmp(env->key, str, len_key) == 0
-				&& str[len_key] == ENV_SEP)
+				&& str[len_key] == ENV_SEP
+				&& env->key[len_key] == '\0')
 				return (obj);
 		}
 		else
 		{
-			if (ft_strncmp(env->key, str, len_key) == 0)
+			if (ft_strcmp(env->key, str) == 0)
 				return (obj);
 		}
 		obj = obj->next;
