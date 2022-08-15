406ba1ca67d8252dce54a2c70483d4002f0c3be7
Anthony BARRIER <abarrier@e1r5p16.clusters.42paris.fr>
2022-08-15 12:18:50 +0200

    UPDT - change logical check to open outfilefor fd and hd to get the correct error message
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/fd/fd_outfile_open.c
    	modified:   srcs/heredoc/hd_open.c

--- srcs/fd/fd_outfile_open.c
+++ srcs/fd/fd_outfile_open.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 18:01:57 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/21 18:18:00 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/15 12:06:46 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ int	fd_outfile_open(t_cmd *cmd, char *fd, int mode)
 	cmd->fd_w = fd_open(fd, mode);
 	if (cmd->fd_w < 0)
 	{
-		if (access(fd, F_OK) != 0)
+		if (access(fd, F_OK) == 0)
 		{
 			cmd->fd_w = fd_open(fd, mode);
 			if (cmd->fd_w < 0)
 				return (ft_shell_msg(errno, fd));
 		}
-		else if (access(fd, O_DIRECTORY) != 0)
+		else if (access(fd, O_DIRECTORY) == 0)
 			return (ft_shell_msg(EISDIR, fd));
 		else
 			return (fd_access(fd, 2));
