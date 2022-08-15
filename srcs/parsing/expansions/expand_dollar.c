2146c5c45050751b27e3caa680206fd586a9e90a
Alicia MARCHAND <amarchan@e1r4p15.clusters.42paris.fr>
2022-08-15 12:40:01 +0200

    ADD - static function "just_dollar_between_quotes" FIX - echo '$' and echo "$"
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/expansions/expand_dollar.c

--- srcs/parsing/expansions/expand_dollar.c
+++ srcs/parsing/expansions/expand_dollar.c
@@ -6,11 +6,32 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 19:47:06 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/15 11:43:01 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/15 12:18:48 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	just_dollar_between_quotes(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			if (i >= 1 && str[i - 1] == '\''
+				&& i + 1 < ft_strlen(str) && str[i + 1] == '\'')
+				return (1);
+			if (i >= 1 && str[i - 1] == '\"'
+				&& i + 1 < ft_strlen(str) && str[i + 1] == '\"')
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
 
 char	*expand_dollar(t_exp_arg exp_arg, int **tab)
 {
@@ -23,7 +44,8 @@ char	*expand_dollar(t_exp_arg exp_arg, int **tab)
 	expanded_list = NULL;
 	malloc = 0;
 	find_expansions(exp_arg, &expanded_list, &full_size);
-	if (ft_strstr(exp_arg.str, "$\'") || ft_strstr(exp_arg.str, "$\""))
+	if ((ft_strstr(exp_arg.str, "$\'") || ft_strstr(exp_arg.str, "$\""))
+		&& !just_dollar_between_quotes(exp_arg.str))
 	{
 		exp_arg.str = remove_dollars(exp_arg.str);
 		if (!exp_arg.str)
