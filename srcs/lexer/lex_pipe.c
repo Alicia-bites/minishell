763791b3a3a3b89e69c9a3b25bbda1459a2f105b
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-08-16 12:01:35 +0200

    ADD - condition to check if pipes are between quotes in case double pipe is found
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/lexer/lex_pipe.c

--- srcs/lexer/lex_pipe.c
+++ srcs/lexer/lex_pipe.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 10:50:33 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/12 16:41:26 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/16 11:59:21 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int	lex_pipe(char *str, int *err)
 	}
 	while (str[i])
 	{
-		if ((str[i] == '|' && str[i + 1] == '|')
-			|| (str[i] == '|' && ft_isspace(str[i + 1]) && str[i + 2] == '|'))
+		if (!between_quotes(str, i) && ((str[i] == '|' && str[i + 1] == '|')
+			|| (str[i] == '|' && ft_isspace(str[i + 1]) && str[i + 2] == '|')))
 		{
 			*err = DOUBLE_PIPE;
 			ft_putendl_fd(MSGERR_PIPE, 2);
