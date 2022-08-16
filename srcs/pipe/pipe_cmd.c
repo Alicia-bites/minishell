6a1d9e81292e6992e3910115cbeaae92229bcf9b
Anthony BARRIER <abarrier@e1r4p1.clusters.42paris.fr>
2022-08-16 19:13:01 +0200

    FIX - fix issue on builtin exectued in a pipe
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   pipe_cmd.c

--- srcs/pipe/pipe_cmd.c
+++ srcs/pipe/pipe_cmd.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:39:27 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/02 17:18:35 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/16 18:58:32 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	pipe_cmd(t_ulist **cmd_lst, t_ulist *obj)
 
 	cmd = (t_cmd *)obj->content;
 	res = 0;
+	errno = 0;
 	envp = env_char_set(cmd->env_lst);
 	pipe_cmd_dup_fd_in(cmd_lst, cmd);
 	pipe_cmd_dup_fd_out(cmd_lst, cmd);
@@ -31,12 +32,9 @@ void	pipe_cmd(t_ulist **cmd_lst, t_ulist *obj)
 		execve(cmd->fullcmd[0], cmd->fullcmd, envp);
 		res = 1;
 	}
-	if (res || errno)
-	{
-		if (cmd->access == -1)
-			errno = ft_panic_value(-1, __FILE__, ERR_CMD_FOUND, 127);
-		else
-			ft_shell_msg(errno, cmd->fullcmd[0]);
-	}
+	if (cmd->toktype == TOK_CMD && (res || errno) && cmd->access == -1)
+		errno = ft_panic_value(-1, __FILE__, ERR_CMD_FOUND, 127);
+	else if (cmd->toktype == TOK_CMD && (res || errno) && cmd->access != -1)
+		ft_shell_msg(errno, cmd->fullcmd[0]);
 	pipe_cmd_end(cmd_lst, cmd, errno, envp);
 }
