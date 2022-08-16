35c1450796220829186f88a506b4cbd5404fb883
Anthony BARRIER <abarrier@e1r4p1.clusters.42paris.fr>
2022-08-16 16:36:00 +0200

    FIX - fix issue when a file with the name as command name exists
    BODY:
    Change order of getvalidpath
    1- Check PATH environment variable
    2- Check other environment variable
    3- Check current directory
    
    TODO:
    N/A
    
    	modified:   cmd_getvalidpath.c

--- srcs/cmd/cmd_getvalidpath.c
+++ srcs/cmd/cmd_getvalidpath.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:01:09 by abarrier          #+#    #+#             */
-/*   Updated: 2022/07/12 18:03:04 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/16 16:12:27 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,14 @@ char	*cmd_getvalidpath(t_cmd *cmd)
 
 	path = NULL;
 	cmd->access = 0;
+	path = cmd_loop_envp_str(cmd->fullcmd[0], cmd->env_lst, ENV_PATH_NAME);
+	if (path)
+		return (path);
+	path = cmd_loop_envp(cmd->fullcmd[0], cmd->env_lst);
+	if (path)
+		return (path);
 	if (access(cmd->fullcmd[0], F_OK & X_OK) == 0)
 		path = ft_strdup(cmd->fullcmd[0]);
-	else
-	{
-		path = cmd_loop_envp_str(cmd->fullcmd[0], cmd->env_lst,
-				ENV_PATH_NAME);
-		if (path)
-			return (path);
-		path = cmd_loop_envp(cmd->fullcmd[0], cmd->env_lst);
-		if (path)
-			return (path);
-	}
 	if (!path)
 	{
 		path = ft_strdup(cmd->fullcmd[0]);
