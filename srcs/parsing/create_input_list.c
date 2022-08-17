d7cab32949f462d6f84fe26be7e66a503dbe1ef8
Alicia MARCHAND <amarchan@e1r5p22.clusters.42paris.fr>
2022-08-17 14:40:17 +0200

    RM - printf
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/parsing/create_input_list.c

--- srcs/parsing/create_input_list.c
+++ srcs/parsing/create_input_list.c
@@ -6,7 +6,7 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 14:43:07 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/17 12:15:00 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/17 14:27:07 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	create_input_list(t_chartype **input_list, char *str)
 	int			length;
 	int			i;
 
-	// printf("str = %s\n", str);
 	length = ft_strlen(str);
 	(*input_list) = malloc(sizeof(t_chartype) * (length + 1));
 	if (!(*input_list))
