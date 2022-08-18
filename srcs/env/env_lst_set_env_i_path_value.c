603ee3d8776e01c552cd51929f7b5767b080e474
Anthony BARRIER <abarrier@e1r6p21.clusters.42paris.fr>
2022-08-18 20:19:06 +0200

    UPDT - teste to debug copying PATH ENV MACRO
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/env/env_lst_set_env_i.c
    	modified:   srcs/env/env_lst_set_env_i_path_value.c

--- srcs/env/env_lst_set_env_i_path_value.c
+++ srcs/env/env_lst_set_env_i_path_value.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/18 14:29:19 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/18 15:36:12 by abarrier         ###   ########.fr       */
+/*   Created: 2022/08/18 16:57:56 by abarrier          #+#    #+#             */
+/*   Updated: 2022/08/18 19:52:45 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ char	*env_lst_set_env_i_path_value(void)
 	size_t	len_p2;
 	size_t	len_p3;
 
-	len_p1 = ENV_PATH_DFT_LEN1;
-	len_p2 = ENV_PATH_DFT_LEN2;
-	len_p3 = ENV_PATH_DFT_LEN3;
+	len_p1 = (size_t)ENV_PATH_DFT_LEN1;
+	len_p2 = (size_t)ENV_PATH_DFT_LEN2;
+	len_p3 = (size_t)ENV_PATH_DFT_LEN3;
+	printf("ENV P1: %s\n", ENV_PATH_DFT_VAL1);
+	printf("ENV P2: %s\n", ENV_PATH_DFT_VAL2);
+	printf("ENV P3: %s\n", ENV_PATH_DFT_VAL3);
 	str = (char *)malloc(sizeof(char) * (len_p1 + len_p2 + len_p3 + 1));
 	if (!str)
 		return (ft_panic_null(-1, __FILE__, ERR_MALLOC));
