d32738cfca9fb3f674d5885b56ae1cfe66f01b82
Anthony BARRIER <abarrier@e1r4p1.clusters.42paris.fr>
2022-08-19 16:11:27 +0200

    FIX - remove free of fd_name because it will be done by ft_lstclear (token list free process)
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/fd/fd_init_tokfile.c

--- srcs/fd/fd_init_tokfile.c
+++ srcs/fd/fd_init_tokfile.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 07:48:13 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/19 11:23:25 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/19 16:10:17 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,7 @@ void	fd_init_tokfile(t_ulist **cmd_lst, t_list *tok, int mode)
 	cmd = fd_init_tokfile_find_cmd(cmd_lst, tok);
 	fd = fd_open(fd_name, mode);
 	if (cmd && fd_init_tokfile_link_cmd_secure(cmd, fd))
-	{
-		free(fd_name);
 		return ;
-	}
 	res_open = fd_init_tokfile_access(fd, fd_name, mode);
 	fd_init_tokfile_link_cmd(cmd, fd, mode, NULL);
 }
