b2744da6dd7e7ff6151b0ceeb839714a7c7dad6a
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-08-19 11:12:47 +0200

    FIX - ctrl-c displayed on same line as heredoc line.
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/signal_handling/sig_hd_sigint.c
    	modified:   srcs/signal_handling/sig_program_prompt_back_hd.c
    	modified:   srcs/signal_handling/sig_program_set_action_hd.c

--- srcs/signal_handling/sig_program_set_action_hd.c
+++ srcs/signal_handling/sig_program_set_action_hd.c
@@ -6,15 +6,15 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 14:07:42 by amarchan          #+#    #+#             */
-/*   Updated: 2022/08/18 14:11:53 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/08/19 10:28:37 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+// #include "minishell.h"
 
-int	sig_program_set_action_hd(void)
-{
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGINT, &sig_program_prompt_back_hd);
-	return (0);
-}
+// int	sig_program_set_action_hd(void)
+// {
+// 	signal(SIGQUIT, SIG_IGN);
+// 	signal(SIGINT, &sig_program_prompt_back_hd);
+// 	return (0);
+// }
