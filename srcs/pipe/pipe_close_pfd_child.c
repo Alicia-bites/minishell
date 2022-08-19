b3c87bf748a6f52cc1e462111aba5fe1f490718f
Alicia MARCHAND <amarchan@e1r4p3.clusters.42paris.fr>
2022-08-19 11:25:46 +0200

    NORM -
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modified:   srcs/pipe/pipe_close_pfd_child.c

--- srcs/pipe/pipe_close_pfd_child.c
+++ srcs/pipe/pipe_close_pfd_child.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_close_pfd_child.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:30:05 by abarrier          #+#    #+#             */
-/*   Updated: 2022/08/19 09:21:14 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/08/19 11:21:32 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	ft_close(int *fd)
+{
+	close(*fd);
+	*fd = FD_NOT_INIT;
+}
 
 void	pipe_close_pfd_child(void *content)
 {
@@ -22,23 +28,11 @@ void	pipe_close_pfd_child(void *content)
 	if (!cmd)
 		return ;
 	if (cmd->fd_r > 2)
-	{
-		close(cmd->fd_r);
-		cmd->fd_r = FD_NOT_INIT;
-	}
+		ft_close(&cmd->fd_r);
 	if (cmd->fd_w > 2)
-	{
-		close(cmd->fd_w);
-		cmd->fd_w = FD_NOT_INIT;
-	}
+		ft_close(&cmd->fd_w);
 	if (cmd->pfd_r > 2)
-	{
-		close(cmd->pfd_r);
-		cmd->pfd_r = FD_NOT_INIT;
-	}
+		ft_close(&cmd->pfd_r);
 	if (cmd->pfd_w > 2)
-	{
-		close(cmd->pfd_w);
-		cmd->pfd_w = FD_NOT_INIT;
-	}
+		ft_close(&cmd->pfd_w);
 }
